aad5e04f570a03c51bf3d68bf3286681863c3b8c
Steven Yang <yangchenyun@gmail.com>
2012-09-12 18:10:15 +0800

    Added validators for the Products Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :name_en, :name_cn, :count_on_hand, :presence => true
+
+  before_save do |product|
+    product.name_en.downcase!
+  end
 
   attr_accessible :name_cn, :name_en, :description, :meta_description, :meta_keywords, :count_on_hand, :cost_price, :price, :height, :width, :depth, :available_on, :deleted_at
 
