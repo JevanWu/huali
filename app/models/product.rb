edc32496c18b51ba7881c94b5387b459428134db
Steven Yang <yangchenyun@gmail.com>
2012-09-13 21:42:38 +0800

    admin:product now could remove assets Render a remove link from view Use javascript to set hidden fields and hide the DOM fielset

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   attr_accessible :name_cn, :name_en, :description, :meta_description, :meta_keywords, :count_on_hand, :cost_price, :price, :height, :width, :depth, :available_on, :deleted_at, :assets, :assets_attributes
 
   has_many :assets, :as => :viewable, :dependent => :destroy
-  accepts_nested_attributes_for :assets
+  accepts_nested_attributes_for :assets, :reject_if => lambda { |a| a[:image].blank? }, :allow_destroy => true
 
   validates :name_en, :name_cn, :count_on_hand, :presence => true
 
