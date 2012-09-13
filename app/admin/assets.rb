9a5b81f85f9eac784ca7c47015f426ee1d109206
Steven Yang <yangchenyun@gmail.com>
2012-09-13 21:56:57 +0800

    Rewrite the assets show page to display more info fixed #7

--- app/admin/assets.rb
+++ app/admin/assets.rb
@@ -3,10 +3,42 @@ ActiveAdmin.register Asset do
   form :html => { :enctype => "multipart/form-data" } do |f|
     f.inputs "Details" do
       f.input :image, :as => :file
-      f.input :viewable_id
       f.input :viewable_type
     end
     f.buttons
+  end
+
+  show do |asset|
+    attributes_table do
+      row :viewable_type
+
+      row :file_name do
+        asset.image_file_name
+      end
+
+      row :file_size do
+        number_to_human_size(asset.image_file_size)
+      end
+
+      row :content_type do
+        asset.image_content_type
+      end
+
+
+      row :image_updated_at
+
+      row :thumbnails do
+        image_tag asset.image.url(:thumb)
+      end
+
+      row :medium_image do
+        image_tag asset.image.url(:medium)
+      end
+
+      row :full_image do
+        image_tag asset.image.url
+      end
+    end
   end
 
 end
