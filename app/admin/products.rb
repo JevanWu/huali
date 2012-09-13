53e5115095977c3a59d3f9636d25a2277b80038e
Steven Yang <yangchenyun@gmail.com>
2012-09-13 15:47:12 +0800

    make admin/product renders nested form to assets used accepts_nested_attributes_for to create assets

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -18,27 +18,5 @@ ActiveAdmin.register Product do
   default_actions
   end
 
-  form do |f|
-    f.inputs "Main Info" do
-      f.input :name_cn, :label => "Chinese Name", :required => true
-      f.input :name_en, :label => "English Name", :required => true
-      f.input :description, :hint => "Use Markdown to write the content"
-    end
-    f.inputs "SKU Info" do
-      f.input :count_on_hand, :label => "Count", :required => true
-      f.input :price
-      f.input :cost_price
-      f.input :height
-      f.input :width
-      f.input :depth
-      f.input :available_on, :as => :date_select
-      f.input :deleted_at, :as => :date_select
-    end
-    f.inputs "SEO" do
-      f.input :meta_keywords
-      f.input :meta_description
-    end
-    f.buttons
-  end
-
+  form :partial => "form"
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name_cn, :name_en, :description, :meta_description, :meta_keywords, :count_on_hand, :cost_price, :price, :height, :width, :depth, :available_on, :deleted_at
+  attr_accessible :name_cn, :name_en, :description, :meta_description, :meta_keywords, :count_on_hand, :cost_price, :price, :height, :width, :depth, :available_on, :deleted_at, :assets, :assets_attributes
 
-  has_many :assets, :as => :viewable
+  has_many :assets, :as => :viewable, :dependent => :destroy
+  accepts_nested_attributes_for :assets
 
   validates :name_en, :name_cn, :count_on_hand, :presence => true
 
