d7deb8bcf87a03e4fd954b72e93ee7f866739969
Steven Yang <yangchenyun@gmail.com>
2012-09-19 18:04:55 +0800

    Updated the product_part admin MVC change has_many to has_one relationship to assets Setup edit/show/index actions

--- app/models/product_part.rb
+++ app/models/product_part.rb
@@ -1,10 +1,10 @@
 class ProductPart < ActiveRecord::Base
-  attr_accessible :description, :name_cn, :name_en, :assets_attributes
+  attr_accessible :description, :name_cn, :name_en, :asset_attributes, :product_id
 
   belongs_to :product
 
-  has_many :assets, :as => :viewable, :dependent => :destroy
-  accepts_nested_attributes_for :assets, :reject_if => lambda { |a| a[:image].blank? }, :allow_destroy => true
+  has_one :asset, :as => :viewable, :dependent => :destroy
+  accepts_nested_attributes_for :asset, :reject_if => lambda { |a| a[:image].blank? }, :allow_destroy => true
 
   validates :name_cn, :name_en, :presence => true
 
