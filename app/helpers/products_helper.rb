5d6065b592f7d0fbcfa9858def5a3e0a822c55da
Steven Yang <yangchenyun@gmail.com>
2012-09-19 22:27:03 +0800

    Enable create product part directly from product close #18 Use some hackings to render the nested asset.image fields

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -7,4 +7,13 @@ module ProductsHelper
     end
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub('\n', '')})
   end
+
+  def link_to_add_parts(name, f, association)
+    new_part = ProductPart.new
+    id = new_part.object_id
+    fields = f.semantic_fields_for(association, new_part, child_index: id) do |builder|
+      render('part_fields', f: builder, id: id)
+    end
+    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub('\n', '')})
+  end
 end
