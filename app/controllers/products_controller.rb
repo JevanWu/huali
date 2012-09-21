afa30dae86ddc29dacb5d3d01632e868425f1e60
zheng zhao <zach.bstek@gmail.com>
2012-09-21 14:38:10 +0800

    unescape the translated html

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,9 +16,8 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @product = Product.find(params[:id])
-    @product.description = Kramdown::Document.new(@product.description).to_html
-    puts "this is fucking good"
-    puts @product.description
+    md2html = Kramdown::Document.new(@product.description).to_html.html_safe
+    @product.description = md2html
 
     respond_to do |format|
       format.html # show.html.erb
