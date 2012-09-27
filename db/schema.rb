ce893688f1a9f010045d04cb9e17102dc41b0bd7
Zach Zhao <zach.bstek@gmail.com>
2012-09-27 13:26:44 +0800

    change info_source column to text type and adjust the field height to 10 rows

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120925055632) do
+ActiveRecord::Schema.define(:version => 20120927051635) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -66,6 +66,16 @@ ActiveRecord::Schema.define(:version => 20120925055632) do
     t.string   "name_en",     :null => false
   end
 
+  create_table "pages", :force => true do |t|
+    t.string   "name"
+    t.string   "permalink"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "pages", ["permalink"], :name => "index_pages_on_permalink"
+
   create_table "product_parts", :force => true do |t|
     t.string   "name_cn",     :null => false
     t.string   "name_en",     :null => false
@@ -73,7 +83,7 @@ ActiveRecord::Schema.define(:version => 20120925055632) do
     t.integer  "product_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.string   "info_source"
+    t.text     "info_source"
   end
 
   create_table "products", :force => true do |t|
@@ -94,7 +104,7 @@ ActiveRecord::Schema.define(:version => 20120925055632) do
     t.integer  "collection_id"
     t.string   "place"
     t.string   "usage"
-    t.string   "info_source"
+    t.text     "info_source"
   end
 
   add_index "products", ["name_en"], :name => "index_products_on_name_en"
