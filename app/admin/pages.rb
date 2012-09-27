9bcb2689c1dc29d8ea2ef0b65959c5ea86f9b417
Zach Zhao <zach.bstek@gmail.com>
2012-09-27 10:49:47 +0800

    override admin pages' actions to prettify url

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,11 +1,29 @@
 ActiveAdmin.register Page do
   controller do
+    def new
+      @page = Page.new
+    end
+
+    def create
+      @page = Page.new(params[:page])
+      if @page.save
+        redirect_to admin_pages_path
+      else
+        render action: "new"
+      end
+    end
+
     def show
       @page = Page.find_by_permalink!(params[:id])
     end
 
     def update
       @page = Page.find_by_permalink!(params[:id])
+      if @page.update_attributes(params[:page])
+        redirect_to admin_page_path 
+      else
+        render action: "edit"
+      end
     end
 
     def edit
@@ -14,6 +32,8 @@ ActiveAdmin.register Page do
 
     def destroy
       @page = Page.find_by_permalink!(params[:id])
+      @page.destroy
+      redirect_to admin_pages_path
     end
   end
 end
