6fc3111e4c961690fe8fd45c1d37c7b3227dadb5
Steven Yang <yangchenyun@gmail.com>
2012-10-17 00:15:19 +0800

    use one line after hooks

--- config/recipes/base.rb
+++ config/recipes/base.rb
@@ -9,7 +9,15 @@ namespace :deploy do
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install python-software-properties"
   end
-  after "deploy:install", "nginx:install", "nodejs:install", "rbenv:install", "dev_lib:install"
+  after "deploy:install",
+    "nginx:install",
+    "nodejs:install",
+    "rbenv:install",
+    "dev_lib:install"
+    "postgresql:install"
 
   # after "deploy:setup", "nginx:setup"
+  # after "deploy:setup", "postgresql:create_database"
+  # after "deploy:setup", "postgresql:setup"
+  # after "deploy:finalize_update", "postgresql:symlink"
 end
