688fe56de53e2f2b0c696fb5a6c40c59d07cb3d4
Steven Yang <yangchenyun@gmail.com>
2012-10-18 11:42:53 +0800

    updated the routes to fix path issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Changanhua::Application.routes.draw do
 
   ActiveAdmin.routes(self)
 
-  match '/products/:id', :to => 'products#show'
+  resources :products, :only => [:show]
 
   # non-individual collections routes
   match '/collections/all', :to => 'collections#all'
   match '/collections/:id', :to => 'collections#show'
+
+  resources :collections, :only => [:show]
 
   devise_for :administrators
 
@@ -20,5 +22,6 @@ Changanhua::Application.routes.draw do
   get ':id', to: 'pages#show', as: :page
   get "errors/error_404"
   get "errors/error_500"
+
 
 end
