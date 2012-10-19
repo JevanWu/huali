3d55e7e7d5b787e3011950df28eb6eabe33d8dd8
zheng zhao <zach.bstek@gmail.com>
2012-10-19 10:35:13 +0800

    remove trailing whitespace

--- db/migrate/20121019022323_change_description2_to_text_type.rb
+++ db/migrate/20121019022323_change_description2_to_text_type.rb
@@ -1,9 +1,9 @@
 class ChangeDescription2ToTextType < ActiveRecord::Migration
   def up
-    change_column :products, :description2, :text 
+    change_column :products, :description2, :text
   end
 
   def down
-    change_column :products, :description2, :string 
+    change_column :products, :description2, :string
   end
 end
