efde50ecabce7c43d0d057a0836fb285ff85644b
Steven Yang <yangchenyun@gmail.com>
2012-10-23 20:39:43 +0800

    use release_path instead of current_path as current is create after database symlink

--- config/recipes/postgresql.rb
+++ config/recipes/postgresql.rb
@@ -31,6 +31,6 @@ namespace :pg do
 
   desc "Symlink the database.yml file into latest release"
   task :symlink, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{current_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
 end
