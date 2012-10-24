c1bc90b7f8ba74462bdce0914f53b2fe3a1aa9f4
Steven Yang <yangchenyun@gmail.com>
2012-10-24 16:52:42 +0800

    compare the local master with the remote master in check

--- config/recipes/check.rb
+++ config/recipes/check.rb
@@ -1,7 +1,7 @@
 namespace :check do
   desc "Make sure local git is in sync with remote."
   task :revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
+    unless `git rev-parse #{branch}` == `git rev-parse origin/#{branch}`
       puts "WARNING: HEAD is not the same as origin/#{branch}"
       puts "Run `git push` to sync changes."
       exit
