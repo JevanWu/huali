f193bd44d7927172e64f558bd7806d502b3af47d
Steven Yang <yangchenyun@gmail.com>
2012-10-24 17:32:17 +0800

    Added staging configuration - separate staging and production server settings - use remote_cache instead of checkout as the deploy method

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,34 +6,47 @@ load "config/recipes/nodejs"
 load "config/recipes/rbenv"
 load "config/recipes/check"
 
-set :application, "changanhua"
-set :domain, "www.changanflowers.com changanflowers.com"
-
 # Use Git as Version Control System
 set :scm, :git
 set :repository, "git@github.com:zenhacks/changanhua.git"
 set :branch, 'master'
 
-# HTTP Server & App Server & DB Server resides at emoo
-# lua - 42.121.3.105, aliyun - steven
-# server '42.121.3.105:1982', :web, :app, :db, :primary => true
-
-# emoo - 74.207.254.157, linode tokyo
-server '74.207.254.157:1982', :web, :app, :db, :primary => true
-
-set :user, 'deploy'
-set :group, 'deploy'
-set :deploy_to, "/home/#{user}/repositories/#{application}"
+# keep a remote cache to avoid checking out everytime
+set :deploy_via, :remote_cache
 
 # enable prompt for password
 default_run_options[:pty] = true
 
 # try_sudo will not use sudo by default
-#
 set :use_sudo, false
 
 # access github.com using as the local user
 ssh_options[:forward_agent] = true
+
+set :application, "changanhua"
+
+# multistage settings
+task :production do
+  set :domain, "www.changanflowers.com changanflowers.com"
+
+  # emoo - 74.207.254.157, linode tokyo
+  server '74.207.254.157:1982', :web, :app, :db, :primary => true
+
+  set :user, 'deploy'
+  set :group, 'deploy'
+  set :deploy_to, "/home/#{user}/repositories/#{application}-production"
+end
+
+task :staging do
+  set :domain, "staging.changanflowers.com"
+
+  # lua - 42.121.3.105, aliyun - steven
+  server '42.121.3.105:1982', :web, :app, :db, :primary => true
+  
+  set :user, 'deployer'
+  set :group, 'admin'
+  set :deploy_to, "/home/#{user}/repositories/#{application}-staging"
+end
 
 # start/stop/restart application
 namespace :deploy do
