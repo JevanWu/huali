3a2a48d1dde9d27f479314e5d1519de0ddd4d306
Steven Yang <yangchenyun@gmail.com>
2012-11-02 19:04:32 +0800

    add unique constrains on post_code fields

--- db/migrate/20121102094727_create_prov_city_area.rb
+++ db/migrate/20121102094727_create_prov_city_area.rb
@@ -17,8 +17,8 @@ class CreateProvCityArea < ActiveRecord::Migration
       t.integer :parent_post_code #foreign_key
     end
 
-    add_index :provinces, [:post_code], :name => 'index_provinces_on_post_code'
-    add_index :cities, [:post_code], :name => 'index_cities_on_post_code'
-    add_index :areas, [:post_code], :name => 'index_areas_on_post_code'
+    add_index :provinces, [:post_code], :name => 'index_provinces_on_post_code', :unique => true
+    add_index :cities, [:post_code], :name => 'index_cities_on_post_code', :unique => true
+    add_index :areas, [:post_code], :name => 'index_areas_on_post_code', :unique => true
   end
 end
