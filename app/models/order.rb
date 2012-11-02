cc34389c8ade41f88df5e63f8fd5f6efb4bb5c86
Steven Yang <yangchenyun@gmail.com>
2012-11-02 21:02:00 +0800

    updated the order model with associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,19 @@
 class Order < ActiveRecord::Base
+
+  attr_accessible :line_items, :address_attributes, :payments_attributes,
+                  :line_items_attributes, :address, :number, :special_instructions
+
   belongs_to :address
+
   belongs_to :user
-  attr_accessible :completed_at, :item_total, :number, :payment_state, :payment_total, :shipment_state, :special_instructions, :state, :total
+
+  has_many :line_items, :dependent => :destroy, :order => "created_at ASC"
+  has_many :payments, :dependent => :destroy
+  has_many :shipments, :dependent => :destroy
+
+  accepts_nested_attributes_for :line_items
+  accepts_nested_attributes_for :address
+  accepts_nested_attributes_for :payments
+  accepts_nested_attributes_for :shipments
+
 end
