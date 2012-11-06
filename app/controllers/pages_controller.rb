9f24e9c70d453098d07d6f33dbd2b84a4f1e4e93
zheng zhao <zach.bstek@gmail.com>
2012-11-06 11:43:19 +0800

    complete the mapping, generate the unique order_num

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   ALIPAY_KEY = "ux04rwiwzqbuksk0xm70u1fvmoo2p32d"
   ALIPAY_PID = "2088801670489935"
   ALIPAY_EMAIL = "tzgbusiness@gmail.com"
-  caches_page :order
-  caches_page :payment
   # GET /pages/1
   # GET /pages/1.json
   def show
@@ -17,7 +15,12 @@ class PagesController < ApplicationController
 
   def order
     product_id = params["product_id"]
-    product_wufoo_mapping = { "1" => 's7x2z7' }
+    product_wufoo_mapping = { 
+      "1" => 's7x2z7',
+      "2" => 'x7x0k1',
+      "3" => 'k7x0r9',
+      "4" => 'z7p9w5'
+    }
     @wufoo_id = product_wufoo_mapping[product_id]
   end
 
@@ -29,16 +32,16 @@ class PagesController < ApplicationController
 
   def alipay
     product_id = params["product_id"]
+    order_num = Time.now.strftime("%Y%m%H%M%S") 
     product = Product.find(product_id)
     options = { 
       :partner => ALIPAY_PID,
-      :out_trade_no =>"10002", 
+      :out_trade_no => order_num, 
       :total_fee => product.price.to_s, 
       :seller_email => ALIPAY_EMAIL,
       :subject => product.name_cn,
       :body => product.description, 
-      :return_url => "http://localhost:3000/",
-      #:return_url => "http://www.baidu.com",
+      :return_url => "http://www.changanflowers.com",
       :key => ALIPAY_KEY
     }
     redirect_to_alipay_gateway(options)
