e328c45c35f8ee7d7f81fbd2254a182f1a5e913e
Steven Yang <yangchenyun@gmail.com>
2012-11-06 20:24:57 +0800

    updated the image sizes

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,5 @@
 class Asset < ActiveRecord::Base
   belongs_to :viewable, :polymorphic => true
   attr_accessible :image, :viewable_type
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"}
+  has_attached_file :image, :styles => { :medium => "310x300>", :thumb => "93x90>"}
 end
