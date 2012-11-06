16c69daafa98f4573586addc996258770d4c0bfc
zheng zhao <zach.bstek@gmail.com>
2012-11-06 10:51:10 +0800

    payment and alipay controller to handle the payment flow

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
+  ALIPAY_KEY = "ux04rwiwzqbuksk0xm70u1fvmoo2p32d"
+  ALIPAY_PID = "2088801670489935"
+  ALIPAY_EMAIL = "tzgbusiness@gmail.com"
   caches_page :order
+  caches_page :payment
   # GET /pages/1
   # GET /pages/1.json
   def show
@@ -12,8 +16,31 @@ class PagesController < ApplicationController
   end
 
   def order
-    @wufoo_id = 's7x2z7'
+    product_id = params["product_id"]
+    product_wufoo_mapping = { "1" => 's7x2z7' }
+    @wufoo_id = product_wufoo_mapping[product_id]
   end
 
-end
 
+  def payment
+    product_id = params["product_id"]
+    @product = Product.find(product_id)
+  end
+
+  def alipay
+    product_id = params["product_id"]
+    product = Product.find(product_id)
+    options = { 
+      :partner => ALIPAY_PID,
+      :out_trade_no =>"10002", 
+      :total_fee => product.price.to_s, 
+      :seller_email => ALIPAY_EMAIL,
+      :subject => product.name_cn,
+      :body => product.description, 
+      :return_url => "http://localhost:3000/",
+      #:return_url => "http://www.baidu.com",
+      :key => ALIPAY_KEY
+    }
+    redirect_to_alipay_gateway(options)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,11 @@ Changanhua::Application.routes.draw do
   end
 
   get 'order', to: 'pages#order', as: :page
+  get 'payment', to: 'pages#payment', as: :page
+  get 'alipay', to: 'pages#alipay', as: :page
+  get 'success', to: 'pages#show', as: :page
   get ':id', to: 'pages#show', as: :page
+  #post 'payment', to: 'pages#alipay', as: :page
   get "errors/error_404"
   get "errors/error_500"
 
