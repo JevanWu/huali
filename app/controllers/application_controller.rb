6ba96e46d5229b1be08bf427e54349fe0497f016
zheng zhao <zach.bstek@gmail.com>
2012-11-06 13:56:04 +0800

    move redirect helper to pages_controller and make it private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-require 'digest/md5'
-require 'open-uri'
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
@@ -23,24 +21,5 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def redirect_to_alipay_gateway(options={})
-    query_string = {
-      :partner => options[:partner],
-      :out_trade_no => options[:out_trade_no],
-      :total_fee => options[:total_fee],
-      :seller_email => options[:seller_email],
-      :return_url => options[:return_url],
-      :body => options[:body],
-      :"_input_charset" => 'utf-8',
-      :service => "create_direct_pay_by_user",
-      :payment_type => "1",
-      :subject => options[:subject]
-    }.sort.map do |key, value|
-      "#{key}=#{URI::encode(value)}" 
-      end.join("&")
-    sign = Digest::MD5.hexdigest(query_string + options[:key])
-    query_string += "&sign=#{sign}&sign_type=MD5"
-    redirect_to "https://www.alipay.com/cooperate/gateway.do?" + query_string
-  end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+require 'digest/md5'
+require 'open-uri'
 class PagesController < ApplicationController
   ALIPAY_KEY = "ux04rwiwzqbuksk0xm70u1fvmoo2p32d"
   ALIPAY_PID = "2088801670489935"
@@ -41,9 +43,35 @@ class PagesController < ApplicationController
       :seller_email => ALIPAY_EMAIL,
       :subject => product.name_cn,
       :body => product.description, 
-      :return_url => "http://www.changanflowers.com",
+      :return_url => "http://hua.li/success?product_id=#{product_id}",
       :key => ALIPAY_KEY
     }
     redirect_to_alipay_gateway(options)
   end
+
+
+  def success
+  end
+  
+  private
+  def redirect_to_alipay_gateway(options={})
+    query_string = {
+      :partner => options[:partner],
+      :out_trade_no => options[:out_trade_no],
+      :total_fee => options[:total_fee],
+      :seller_email => options[:seller_email],
+      :return_url => options[:return_url],
+      :body => options[:body],
+      :"_input_charset" => 'utf-8',
+      :service => "create_direct_pay_by_user",
+      :payment_type => "1",
+      :subject => options[:subject]
+    }.sort.map do |key, value|
+      "#{key}=#{URI::encode(value)}" 
+      end.join("&")
+    sign = Digest::MD5.hexdigest(query_string + options[:key])
+    query_string += "&sign=#{sign}&sign_type=MD5"
+    redirect_to "https://www.alipay.com/cooperate/gateway.do?" + query_string
+  end
+
 end
