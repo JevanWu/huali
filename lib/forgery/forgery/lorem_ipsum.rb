0db9723d947ecb2bd11ab89d1cc29726f7ac23f9
Steven Yang <yangchenyun@gmail.com>
2012-11-29 16:06:34 +0800

    correct the words methods

--- lib/forgery/forgery/lorem_ipsum.rb
+++ lib/forgery/forgery/lorem_ipsum.rb
@@ -2,10 +2,10 @@
 class Forgery::LoremIpsum < Forgery
   protected
     def self.lorem_ipsum_words
-      @@lorem_ipsum_words ||= dictionaries[:words].split("\r")
+      @@lorem_ipsum_words ||= dictionaries[:words].shuffle()
     end
 
     def self.lorem_ipsum_characters
-      @@lorem_ipsum_characters ||= dictionaries[:words].join("")
+      @@lorem_ipsum_characters ||= dictionaries[:words].shuffle().join("")
     end
 end
