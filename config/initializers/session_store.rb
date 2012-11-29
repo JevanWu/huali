3b0b4bc7856785f5fff58be31d37f2a1ab9e3d73
Steven Yang <yangchenyun@gmail.com>
2012-11-29 17:26:44 +0800

    use redis to store session instead of cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Huali::Application.config.session_store :cookie_store, key: '_changanhua_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Huali::Application.config.session_store :active_record_store
+Huali::Application.config.session_store :redis_store, key: '_huali_session'
