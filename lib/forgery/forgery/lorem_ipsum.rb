eeb83055d1f7eca70fa84f30a524f25c0e540de0
Steven Yang <yangchenyun@gmail.com>
2012-11-29 16:13:53 +0800

    added randomness for sentences / words and characters

--- lib/forgery/forgery/lorem_ipsum.rb
+++ lib/forgery/forgery/lorem_ipsum.rb
@@ -1,11 +1,18 @@
 # encoding: utf-8
 class Forgery::LoremIpsum < Forgery
+
+  def self.sentences(quantity=2, options={})
+    options.merge!(:random_limit => (dictionaries[:lorem_ipsum].length-quantity)) if quantity.is_a?(Fixnum)
+
+    dictionaries[:lorem_ipsum].shuffle()[range_from_quantity(quantity, options)].join(" ")
+  end
+
   protected
     def self.lorem_ipsum_words
-      @@lorem_ipsum_words ||= dictionaries[:words].shuffle()
+      @@lorem_ipsum_words = dictionaries[:words].shuffle()
     end
 
     def self.lorem_ipsum_characters
-      @@lorem_ipsum_characters ||= dictionaries[:words].shuffle().join("")
+      @@lorem_ipsum_characters = dictionaries[:words].shuffle().join("")
     end
 end
