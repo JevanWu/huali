8bb3485fdb9fd0de3d71f6e66d7ba465babd4f37
zheng zhao <zach.bstek@gmail.com>
2012-11-29 09:59:37 +0800

    add banklist comment

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,6 @@
 module PagesHelper
   def choose_payment_method
+    # banks = ['ICBCB2C', 'CMB', 'CCB', 'BOCB2C', 'ABC', 'COMM', 'CEBBANK', 'SPDB', 'SHBANK', 'GDB', 'CITIC', 'CIB', 'SDB', 'CMBC', 'BJBANK', 'HZCBB2C', 'BJRCB', 'SPABANK', 'FDB', 'WZCBB2C-DEBIT', 'NBBANK', 'ICBCBTB', 'CCBBTB', SPDBB2B', 'ABCBTB']
     banks = ['ICBCB2C', 'CMB', 'CCB', 'BOCB2C', 'ABC', 'COMM', 'CEBBANK', 'SPDB', 'SHBANK']
     render :partial => "choose_payment_method", :locals => { :product => @product, :banks => banks }
   end
