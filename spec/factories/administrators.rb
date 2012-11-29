f5aa369dfbeedbcf730e68e8c0ae0fd78d3d9818
Steven Yang <yangchenyun@gmail.com>
2012-11-29 12:10:09 +0800

    updated the admin and user factory

--- spec/factories/administrators.rb
+++ spec/factories/administrators.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :administrator do
     email Forgery(:internet).email_address
     password Forgery(:basic).password
-    password_confirmation password
+    password_confirmation { password }
     # confirmed_at Time.now
   end
 end
