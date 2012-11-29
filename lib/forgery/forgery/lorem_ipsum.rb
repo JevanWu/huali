f714c60311bf14b0147181e4131f553639ee91e6
zheng zhao <zach.bstek@gmail.com>
2012-11-29 12:24:47 +0800

    generate forgery data

--- lib/forgery/forgery/lorem_ipsum.rb
+++ lib/forgery/forgery/lorem_ipsum.rb
@@ -2,8 +2,10 @@
 class Forgery::LoremIpsum < Forgery
   protected
     def self.lorem_ipsum_words
+      @@lorem_ipsum_words ||= dictionaries[:words].split("\r")
     end
 
     def self.lorem_ipsum_characters
+      @@lorem_ipsum_characters ||= dictionaries[:words].join("")
     end
 end
