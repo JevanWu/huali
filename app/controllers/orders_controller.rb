c23d388297439600286627b515dd9559789bc236
Steven Yang <yangchenyun@gmail.com>
2012-12-05 17:55:39 +0800

    init the controller of order, use order as default layout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,22 @@
 class OrdersController < ApplicationController
+  layout 'order'
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def current
+  end
 end
