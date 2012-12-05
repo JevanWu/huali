15dc50d7a8387a6c96546149dac14262b5327ca6
Steven Yang <yangchenyun@gmail.com>
2012-12-05 15:26:48 +0800

    complete the order specs

--- spec/routing/order_spec.rb
+++ spec/routing/order_spec.rb
@@ -10,7 +10,11 @@ describe "routes for Order", type: :routing do
   end
 
   it "route to show historic order" do
-    { get: "/orders/234" }.should route_to "orders#show"
+    { get: "/orders/234" }.should route_to(
+      :controller => "orders",
+      :action => "show",
+      :id => "234"
+    )
   end
 
   it "route to show order creation form" do
@@ -18,10 +22,18 @@ describe "routes for Order", type: :routing do
   end
 
   it "route to action to create order" do
-    { post: "/orders/create" }.should route_to "orders#create"
+    { post: "/orders" }.should route_to "orders#create"
   end
 
   it "doesn't route to action to delete order" do
     { delete: "/orders/234" }.should_not be_routable
+  end
+
+  it "doesn't route to action to edit order" do
+    { get: "/orders/234/edit" }.should_not be_routable
+  end
+
+  it "doesn't route to action to update order" do
+    { put: "/orders/234" }.should_not be_routable
   end
 end
