707df591bd8d60d06524f9c775109e33e42132ac
Steven Yang <yangchenyun@gmail.com>
2012-12-07 21:35:56 +0800

    commit the schema after i18n integration

--- db/schema.rb
+++ db/schema.rb
@@ -107,6 +107,24 @@ ActiveRecord::Schema.define(:version => 20121205033824) do
   add_index "line_items", ["order_id"], :name => "index_line_items_on_order_id"
   add_index "line_items", ["product_id"], :name => "index_line_items_on_product_id"
 
+  create_table "orders", :force => true do |t|
+    t.string   "number"
+    t.decimal  "item_total",           :precision => 8, :scale => 2, :default => 0.0, :null => false
+    t.decimal  "total",                :precision => 8, :scale => 2, :default => 0.0, :null => false
+    t.decimal  "payment_total",        :precision => 8, :scale => 2, :default => 0.0
+    t.string   "state"
+    t.string   "payment_state"
+    t.string   "shipment_state"
+    t.text     "special_instructions"
+    t.integer  "address_id"
+    t.integer  "user_id"
+    t.datetime "completed_at"
+    t.datetime "created_at",                                                          :null => false
+    t.datetime "updated_at",                                                          :null => false
+  end
+
+  add_index "orders", ["number"], :name => "index_orders_on_number"
+
   create_table "pages", :force => true do |t|
     t.string   "title"
     t.string   "permalink"
@@ -154,6 +172,17 @@ ActiveRecord::Schema.define(:version => 20121205033824) do
   end
 
   add_index "provinces", ["post_code"], :name => "index_provinces_on_post_code", :unique => true
+
+  create_table "settings", :force => true do |t|
+    t.string   "var",                      :null => false
+    t.text     "value"
+    t.integer  "thing_id"
+    t.string   "thing_type", :limit => 30
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
+  end
+
+  add_index "settings", ["thing_type", "thing_id", "var"], :name => "index_settings_on_thing_type_and_thing_id_and_var", :unique => true
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
