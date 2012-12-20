bb4166ed4c1026c6cb06e16c4259d1b9ffeb5268
Steven Yang <yangchenyun@gmail.com>
2012-12-20 18:07:18 +0800

    reorganized the alipay gateway

--- lib/billing/alipay/gateway.rb
+++ lib/billing/alipay/gateway.rb
@@ -1,6 +1,6 @@
 module Billing
   module Alipay
-    class Gateway
+    class Gateway < Billing::Gateway
       SERVICE_URL = "https://www.alipay.com/cooperate/gateway.do?"
 
       DEFAULT_OPTS = {
@@ -9,8 +9,6 @@ module Billing
         "partner" => Alipay::Pid,
         "_input_charset" => "utf-8",
         "service" => "create_direct_pay_by_user",
-        "notify_url" => "http =>//hua.li/notify",
-        "error_notify_url" => "http =>//hua.li/notify_error",
 
         # default options
         "payment_type" => "1",
@@ -24,10 +22,11 @@ module Billing
       # "body" => content,
       # "show_url" => '',
       # "return_url" => "http://hua.dev/success/#{product.id}",
+      # "notify_url" => "http =>//hua.dev/notify",
+      # "error_notify_url" => "http =>//hua.dev/notify_error",
       # "paymethod" => "bankPay",
       # "defaultbank" => 'CMB
       # }
-
       def initialize(options)
         @options = DEFAULT_OPTS.merge(options)
       end
@@ -45,18 +44,6 @@ module Billing
         # check the presence of required params
         # check the type of certain params
         self
-      end
-
-      def query_string
-        compacted_options.sort.map do |key, value|
-            "#{key}=#{value}"
-        end.join("&")
-      end
-
-      def compacted_options
-        @options.select do |key, value|
-          not (value.nil? or value.empty?)
-        end
       end
 
       def add_sign(query)
