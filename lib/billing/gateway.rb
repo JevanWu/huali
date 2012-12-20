8362bc418a754ae2b503640736c596df5dc14160
Steven Yang <yangchenyun@gmail.com>
2012-12-21 01:47:11 +0800

    use blank? instead of empty? and nil check

--- lib/billing/gateway.rb
+++ lib/billing/gateway.rb
@@ -4,13 +4,13 @@ module Billing
 
     def query_string
       compacted_options.sort.map do |key, value|
-          "#{key}=#{value}"
+        "#{key}=#{value}"
       end.join("&")
     end
 
     def compacted_options
       @options.select do |key, value|
-        not (value.nil? or value.empty?)
+        not value.blank?
       end
     end
   end
