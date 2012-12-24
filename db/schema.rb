9df80c642492ec1d96373004b5846315ce83f83d
Steven Yang <yangchenyun@gmail.com>
2012-12-24 16:08:15 +0800

    refactored the database a little

--- db/schema.rb
+++ db/schema.rb
@@ -179,11 +179,9 @@ ActiveRecord::Schema.define(:version => 20121224075413) do
 
   create_table "transactions", :force => true do |t|
     t.string   "identifier"
-    t.string   "trade_no"
     t.string   "merchant_name"
     t.string   "merchant_no"
     t.integer  "order_id"
-    t.string   "service"
     t.integer  "amount"
     t.string   "status"
     t.datetime "processed_at"
