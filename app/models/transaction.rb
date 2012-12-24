e8f8c5860b0a8e05f85757366c1cb5cb16379122
Steven Yang <yangchenyun@gmail.com>
2012-12-24 18:15:37 +0800

    completed the transaction models with validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,20 +1,27 @@
 class Transaction < ActiveRecord::Base
-  attr_accessible :merchant_name, :merchant_trade_no, :amount
+  attr_accessible :merchant_name, :paymethod, :amount, :subject, :body
 
   belongs_to :order, :dependent => :destroy
 
-  validates :identifier, :merchant_name, :amount, :presence => true
-  validates :identifier, :uniqueness => true
-  validates :amount, :numericality => true
-  validates_associated :order
+  before_validation :generate_identifier, on: :create
 
-  before_validation :generate_identifier
+  validates :identifier, :paymethod, :merchant_name, :amount, :subject, presence: true
+  validates :identifier, uniqueness: true
+  validates :amount, numericality: true
+  validates :paymethod, inclusion: {
+    in: %w(paypal directPay bankPay),
+    message: "%{value} is not a valid paymethod."
+
+  }
+  validates :merchant_name, inclusion: {
+    in: %w(Alipay Paypal ICBCB2C CMB CCB BOCB2C ABC COMM CMBC),
+    message: "%{value} is not a valid merchant name."
+  }
+  validates_associated :order
 
   private
 
   def generate_identifier
-    unless @identifier
-      @identifier = Time.now.strftime("%Y%m%H%M%S")
-    end
+    self.identifier ||= Time.now.strftime("%Y%m%H%M%S")
   end
 end
