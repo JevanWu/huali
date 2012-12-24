59343e11c9a17183820feaf62cfb3730dc4cd105
Steven Yang <yangchenyun@gmail.com>
2012-12-24 16:24:21 +0800

    sketched the transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,20 @@
 class Transaction < ActiveRecord::Base
-  attr_accessible :amount, :identifier, :status
+  attr_accessible :merchant_name, :merchant_trade_no, :amount
 
   belongs_to :order, :dependent => :destroy
 
+  validates :identifier, :merchant_name, :amount, :presence => true
+  validates :identifier, :uniqueness => true
+  validates :amount, :numericality => true
+  validates_associated :order
+
+  before_validation :generate_identifier
+
+  private
+
+  def generate_identifier
+    unless @identifier
+      # generate identifier
+    end
+  end
 end
