bba3079af08109f4b5fb02e45f7feac4208f7d2e
Steven Yang <yangchenyun@gmail.com>
2012-12-27 12:47:12 +0800

    added unique indexes on identifiers on transactions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121227042818) do
+ActiveRecord::Schema.define(:version => 20121227044129) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -191,6 +191,9 @@ ActiveRecord::Schema.define(:version => 20121227042818) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
   end
+
+  add_index "transactions", ["identifier"], :name => "index_transactions_on_identifier", :unique => true
+  add_index "transactions", ["order_id"], :name => "index_transactions_on_order_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
