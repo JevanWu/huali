502e0c02e1169839f7d59597fef6335c86c9f92a
Steven Yang <yangchenyun@gmail.com>
2012-12-28 14:36:42 +0800

    added the i18n default before genearte sitemaps

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,27 @@
 # Set the host name for URL creation
+# Put links creation logic here.
+#
+# The root path '/' and sitemap index file are added automatically for you.
+# Links are added to the Sitemap in the order they are specified.
+#
+# Usage: add(path, options={})
+#        (default options are used if you don't specify)
+#
+# Defaults: :priority => 0.5, :changefreq => 'weekly',
+#           :lastmod => Time.now, :host => default_host
+#
+# Examples:
+#
+# Add '/articles'
+#
+#   add articles_path, :priority => 0.7, :changefreq => 'daily'
+#
+# Add all articles:
+#
+#   Article.find_each do |article|
+#     add article_path(article), :lastmod => article.updated_at
+#   end
+
 SitemapGenerator::Sitemap.default_host = "http://www.hua.li"
 
 SitemapGenerator::Sitemap.create do
@@ -6,41 +29,17 @@ SitemapGenerator::Sitemap.create do
   add '/about', :changefreq => 'weekly'
   add '/help-center', :changefreq => 'weekly'
   add '/copyright', :changefreq => 'weekly'
-  Product.unscoped.find_each do |product|
-    
-    product_images = []
 
-    product.assets.each do |asset|
-      product_image = {}
-      product_image[:loc] = asset.image.url(:medium)
-      product_image[:title] = product.name
-      product_images << product_image
+  I18n.locale = :'zh-CN'
+
+  Product.find_each do |product|
+
+    product_images = product.assets.map do |asset|
+      { loc: asset.image.url(:medium), title: product.name }
     end
 
-
-    add(product_path(product), :lastmod => product.updated_at, :images => product_images)
+    add(product_path(product),
+        :lastmod => product.updated_at,
+        :images => product_images)
   end
-
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
 end
