a46ddb0f04bb45227121e3baa02759181b9bc20a
Steven Yang <yangchenyun@gmail.com>
2013-01-05 12:20:59 +0800

    scafolding tests

---
+++ spec/factories/ship_methods.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :ship_method do
+    tracking_num "MyString"
+    name "MyString"
+    service_phone "MyString"
+    type ""
+    website "MyString"
+  end
+end
