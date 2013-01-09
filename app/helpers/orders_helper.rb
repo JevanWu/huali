1ce214dc25a225e6f55ddf844848ad37a8bfab74
Steven Yang <yangchenyun@gmail.com>
2013-01-09 18:46:22 +0800

    implement the table footer counter

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,4 +2,9 @@ module OrdersHelper
   def count_total(item)
     number_to_currency (item[:quantity] * item.price), unit: '&yen;'
   end
+
+  def count_cart(items)
+    item_total = items.inject(0.0) {|sum, item| sum + item[:quantity] * item.price}
+    number_to_currency item_total, unit: '&yen;'
+  end
 end
