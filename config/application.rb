675796692c4a3bb27b18dd69cd78e64491775f4b
Steven Yang <yangchenyun@gmail.com>
2013-01-13 06:21:52 +0800

    let devise use the horizontal layout

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,10 @@ module Huali
 
     end
 
+    config.to_prepare do
+      Devise::SessionsController.layout 'horizontal'
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
