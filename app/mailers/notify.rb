7c5682f8c8fc21cd482cc6934bb9ff2d88d54980
Steven Yang <yangchenyun@gmail.com>
2013-01-18 06:05:59 +0800

    enabled a notify class to send order creation emails

---
+++ app/mailers/notify.rb
@@ -0,0 +1,31 @@
+class Notify < ActionMailer::Base
+  include Resque::Mailer
+  add_template_helper ApplicationHelper
+
+  default_url_options[:host] = 'hua.li'
+  default from: 'info@hua.li'
+
+  #
+  # Order
+  #
+
+  def new_order_user_email(order_id)
+    @order = Order.full_info(order_id)
+    mail(to: 's@zenhacks.org', subject: subject("New Order", @order.subject_text))
+  end
+
+  private
+
+  # Examples
+  #
+  #   >> subject('Lorem ipsum')
+  #   => "Huali | Lorem ipsum"
+  #
+  #   # Accepts multiple arguments
+  #   >> subject('Lorem ipsum', 'Dolor sit amet')
+  #   => "Huali | Lorem ipsum | Dolor sit amet"
+
+  def subject(*extra)
+    "Huali | " << extra.join(' | ')
+  end
+end
