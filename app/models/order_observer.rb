fe77a14ec1da7ff96f931e4828afc1f5f807a4c7
Steven Yang <yangchenyun@gmail.com>
2013-01-18 06:40:47 +0800

    add the observes for create and pay order

--- app/models/order_observer.rb
+++ app/models/order_observer.rb
@@ -1,2 +1,9 @@
 class OrderObserver < ActiveRecord::Observer
+  def after_create(order)
+    Notify.new_order_user_email(order.id).deliver
+  end
+
+  def after_pay
+    Notify.pay_order_user_email(order.id).deliver
+  end
 end
