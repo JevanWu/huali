daf7b27210fee3c35121afc8423da8a8c9749866
Steven Yang <yangchenyun@gmail.com>
2013-01-18 07:58:57 +0800

    add sender email/phone/name on order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114080314) do
+ActiveRecord::Schema.define(:version => 20130117235601) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -121,6 +121,9 @@ ActiveRecord::Schema.define(:version => 20130114080314) do
     t.datetime "updated_at",                                                              :null => false
     t.text     "gift_card_text"
     t.date     "delivery_date",                                                           :null => false
+    t.string   "sender_email"
+    t.string   "sender_phone"
+    t.string   "sender_name"
   end
 
   add_index "orders", ["identifier"], :name => "index_orders_on_identifier", :unique => true
