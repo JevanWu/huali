ca2f9b8e925cfaf624b21321ea421fa99cee81a4
Steven Yang <yangchenyun@gmail.com>
2013-01-19 04:28:55 +0800

    implemented the admin notify emails

--- app/mailers/notify.rb
+++ app/mailers/notify.rb
@@ -5,12 +5,9 @@ class Notify < ActionMailer::Base
   add_template_helper ApplicationHelper
 
   default_url_options[:host] = $host
-  default from: 'info@hua.li'
+  default from: 'support@hua.li'
 
-  #
-  # Order
-  #
-
+  # For User
   def new_order_user_email(order_id)
     @order = Order.full_info(order_id)
     mail(to: @order.sender_email, subject: subject("新订单", @order.subject_text))
@@ -24,6 +21,12 @@ class Notify < ActionMailer::Base
   def ship_order_user_email(order_id)
     @order = Order.full_info(order_id)
     mail(to: @orders.sender_email, subject: subject("订单已发货", @order.subject_text))
+  end
+
+  # For Admin
+  def pay_order_admin_email(order_id)
+    @order = Order.full_info(order_id)
+    mail(to: 's@zenhacks.org', subject: subject("订单付款成功", @order.subject_text))
   end
 
   private
