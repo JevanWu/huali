5fabcffcaecad9296c123c40d28beac805b8066b
Zach Zhao <zach.bstek@gmail.com>
2013-01-20 13:58:34 +0800

    translation for parent menu

--- app/admin/resque.rb
+++ app/admin/resque.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register_page "Resque" do
   controller.authorize_resource
-  menu parent: 'Setting', :priority => 2, if: proc { can? :read, Resque }
+  menu parent: I18n.t('active_admin.menu.setting'), :priority => 2, if: proc { can? :read, Resque }
 end
