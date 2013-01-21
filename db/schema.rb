5fb54aca51e40276aaa835ef7fbae5f3dec195ad
Steven Yang <yangchenyun@gmail.com>
2013-01-21 18:46:17 +0800

    rename the delivery_date column to expected_date

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130121081640) do
+ActiveRecord::Schema.define(:version => 20130121104252) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -122,7 +122,7 @@ ActiveRecord::Schema.define(:version => 20130121081640) do
     t.datetime "created_at",                                                              :null => false
     t.datetime "updated_at",                                                              :null => false
     t.text     "gift_card_text"
-    t.date     "delivery_date",                                                           :null => false
+    t.date     "expected_date",                                                           :null => false
     t.string   "sender_email"
     t.string   "sender_phone"
     t.string   "sender_name"
