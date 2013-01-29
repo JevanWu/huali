0b4fd34e5f83aa21045cdf81d2f0d4aea07827a7
Steven Yang <yangchenyun@gmail.com>
2013-01-29 16:54:05 +0800

    add slug generator in migration

--- db/migrate/20130129084810_add_slug_to_collection.rb
+++ db/migrate/20130129084810_add_slug_to_collection.rb
@@ -1,6 +1,15 @@
+class Collection < ActiveRecord::Base
+end
+
 class AddSlugToCollection < ActiveRecord::Migration
-  def change
+  def up
     add_column :collections, :slug, :string
     add_index :collections, :slug, unique: true
+    Collection.find_each(&:save)
+  end
+
+  def down
+    remove_column :collections, :slug
+    remove_index :collections, :slug
   end
 end
