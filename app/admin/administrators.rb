c56f470d3a1294c40be90b835ff3c6c52252b42e
Zach Zhao <zach.bstek@gmail.com>
2013-01-29 16:26:36 +0800

    ajust active admin page for administrator

--- app/admin/administrators.rb
+++ app/admin/administrators.rb
@@ -1,4 +1,27 @@
 ActiveAdmin.register Administrator do
   menu :priority => 1, if: proc { can? :read, Administrator }
   controller.authorize_resource
+
+  filter :role
+  filter :email
+
+  index do
+    selectable_column
+    column :email
+    column :role
+    column :last_sign_in_at
+    default_actions
+  end
+
+  form :partial => "form"
+
+  show do
+    attributes_table do
+      row :email
+      row :role
+      row :current_sign_in_at
+      row :last_sign_in_at
+      row :sign_in_count
+    end
+  end
 end
