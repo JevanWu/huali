8a395b4f962dfd4ac20405bb90b9f0bb4fe40a9d
Steven Yang <yangchenyun@gmail.com>
2013-01-30 21:28:10 +0800

    added title image on collection page

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,2 +1,14 @@
 module CollectionsHelper
+  def col_title_img(col)
+    case col.id
+    when 8
+      'title-yongsheng.png'
+    when 4
+      'title-dongxi.png'
+    when 10
+      'title-chuncui.png'
+    when 9
+      'title-banlan.png'
+    end
+  end
 end
