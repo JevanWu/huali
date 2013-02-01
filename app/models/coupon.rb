776e0d919d11e4b853d61fc8b855d91322f71312
Steven Yang <yangchenyun@gmail.com>
2013-02-01 17:45:01 +0800

    finished the coupon model

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -15,6 +15,34 @@
 #  coupons_on_code  (code) UNIQUE
 #
 
+require 'securerandom'
+
 class Coupon < ActiveRecord::Base
   attr_accessible :adjustment, :code, :expires_at, :used
+
+  before_validation :generate_code, on: :create
+
+  validates_presence_of :adjustment, :code, :expires_at
+  # +/-/*/%1234.0
+  validates_format_of :adjustment, :with => %r{\A[+-x*%/][\s\d.]+}
+
+  def use!
+    return false unless valid?
+    self.used = true
+    if self.save!
+      return adjustment
+    else
+      return false
+    end
+  end
+
+  def usable?
+    not used && (Time.current < expireds_at)
+  end
+
+  private
+
+  def generate_code
+    self.code = SecureRandom.hex[0...8]
+  end
 end
