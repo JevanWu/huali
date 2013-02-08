47efaacd11a233d37a5207b8ad1dc41a687b352f
Steven Yang <yangchenyun@gmail.com>
2013-02-08 18:05:58 +0800

    change the identifier generator to be shorter

--- lib/active_record_util.rb
+++ lib/active_record_util.rb
@@ -23,12 +23,12 @@ class ActiveRecord::Base
   protected
 
   def uid_prefixed_by(prefix = 'NO')
-    date_string = Time.now.strftime('%Y%m%d')
-    day_uiq_num = "%06d" % $redis.incr("#{self.class.name}:#{date_string}")
+    date_string = Time.now.strftime('%y%m%d')
+    day_uiq_num = "%04d" % $redis.incr("#{self.class.name}:#{date_string}")
 
     # avoid conflicts in identifier, as it is used to communicate with Alipay/Paypal
     unless Rails.env == 'production'
-      prefix = prefix + Rails.env
+      prefix = prefix + Rails.env[0]
     end
     prefix + date_string + day_uiq_num
   end
