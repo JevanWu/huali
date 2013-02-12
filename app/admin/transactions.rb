ae2058a2c084b0a49abf5b3ce4972a6c44c2d8c0
Steven Yang <yangchenyun@gmail.com>
2013-02-12 17:57:41 +0800

    change the transaction filter to a selector

--- app/admin/transactions.rb
+++ app/admin/transactions.rb
@@ -9,7 +9,7 @@ ActiveAdmin.register Transaction do
   end
 
   filter :identifier
-  filter :paymethod
+  filter :paymethod, :as => :select, :collection => { "Paypal" => "paypal", "支付宝" => "directPay", "网上银行" => "bankPay" }
   filter :state, :as => :select, :collection => { "新建" => "generated", "完成" => "completed", "处理中" => "processing", "失败" => "failed" }
   filter :amount
 
