c7c50d4360948bdb069166be83a8210a21ba3c15
Steven Yang <yangchenyun@gmail.com>
2013-02-20 16:52:18 +0800

    added migrating for former data

--- db/migrate/20130220082106_add_ship_method_id_on_order.rb
+++ db/migrate/20130220082106_add_ship_method_id_on_order.rb
@@ -1,5 +1,22 @@
+class Order < ActiveRecord::Base
+end
+
+class Shipment < ActiveRecord::Base
+end
+
 class AddShipMethodIdOnOrder < ActiveRecord::Migration
-  def change
+  def up
     add_column :orders, :ship_method_id, :string
+
+    Order.find_each do |o|
+      if o.shipment
+        o.ship_method_id = o.shipment.ship_method_id
+        o.save
+      end
+    end
+  end
+
+  def down
+    remove_column :orders, :ship_method_id
   end
 end
