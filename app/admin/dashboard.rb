65ffe0b0e5542151d93ee81d7c87c293be4886a5
Steven Yang <yangchenyun@gmail.com>
2013-03-05 11:29:40 +0800

    make the dashboard usagable

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,19 +5,21 @@ ActiveAdmin.register_page "Dashboard" do
     columns do
       column do
         panel "最近进来的订单" do
-          table_for Order.order('id desc').limit(5) do
+          table_for Order.order('id desc').limit(10) do
             column("状态")  { |order| status_tag t('models.order.state.' + order.state), order_state(order) }
-            column("寄件人姓名")  { |order| link_to(order.sender_name, admin_order_path(order)) }
+            column("订单号")  { |order| link_to(order.identifier, admin_order_path(order)) }
+            column("产品")  { |order| order.subject_text }
             column("金额")  { |order| number_to_currency order.item_total }
+            column("下单时间")  { |order| l order.created_at, format: :short }
           end
         end
       end
 
       column do
-        panel "最近登录的用户" do
-          table_for User.order('last_sign_in_at desc').limit(5) do
+        panel "最近注册的用户" do
+          table_for User.non_guests.order('created_at desc').limit(10) do
             column("邮箱")  { |user| link_to user.email, admin_user_path(user) }
-            column("上次登录时间")  { |user| user.last_sign_in_at }
+            column("注册时间")  { |user| l user.created_at, format: :short }
             column("登录次数")  { |user| user.sign_in_count }
           end
         end
