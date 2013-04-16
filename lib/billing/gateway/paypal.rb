be2cfe35f61665a990130575ab4fa8fb0286aaaa
Steven Yang <yangchenyun@gmail.com>
2013-04-16 14:43:32 +0800

    correct the return params to paypal gateway

--- lib/billing/gateway/paypal.rb
+++ lib/billing/gateway/paypal.rb
@@ -29,7 +29,7 @@ module Billing
           charset: "utf-8",
           business: PAYPAL_EMAIL,
           currency_code: "USD",
-          return_url: return_order_url(host: $host || 'localhost') + custom_data,
+          return: return_order_url(host: $host || 'localhost') + custom_data,
           notify_url: notify_order_url(host: $host || 'localhost') + custom_data
         }
       end
