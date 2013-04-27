796e5a1c0d355d733fcbd5667c3ccaf4547995ed
Steven Yang <yangchenyun@gmail.com>
2013-04-27 10:38:25 +0800

    annotate the models
    Conflicts:
    	app/models/product.rb
    	spec/factories/products.rb
    	spec/models/product_spec.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,32 +2,32 @@
 #
 # Table name: products
 #
-#  available            :boolean          default(TRUE)
-#  cost_price           :decimal(8, 2)
-#  count_on_hand        :integer          default(0), not null
-#  created_at           :datetime         not null
-#  depth                :decimal(8, 2)
-#  description_en       :text
-#  description_zh       :text
-#  height               :decimal(8, 2)
-#  id                   :integer          not null, primary key
-#  inspiration_en       :text
-#  inspiration_zh       :text
-#  meta_description     :string(255)
-#  meta_keywords        :string(255)
-#  meta_title           :string(255)
-#  name_char            :string(255)
-#  name_en              :string(255)      default(""), not null
-#  name_zh              :string(255)      default(""), not null
-#  original_price       :decimal(, )
-#  price                :decimal(8, 2)
-#  priority             :integer          default(5)
-#  published_en         :boolean          default(FALSE)
-#  published_zh         :boolean          default(FALSE)
-#  sales_volume_totally :integer          default(0)
-#  slug                 :string(255)
-#  updated_at           :datetime         not null
-#  width                :decimal(8, 2)
+#  available        :boolean          default(TRUE)
+#  cost_price       :decimal(8, 2)
+#  count_on_hand    :integer          default(0), not null
+#  created_at       :datetime         not null
+#  depth            :decimal(8, 2)
+#  description_en   :text
+#  description_zh   :text
+#  height           :decimal(8, 2)
+#  id               :integer          not null, primary key
+#  inspiration_en   :text
+#  inspiration_zh   :text
+#  meta_description :string(255)
+#  meta_keywords    :string(255)
+#  meta_title       :string(255)
+#  name_char        :string(255)
+#  name_en          :string(255)      default(""), not null
+#  name_zh          :string(255)      default(""), not null
+#  original_price   :decimal(, )
+#  price            :decimal(8, 2)
+#  priority         :integer          default(5)
+#  published_en     :boolean          default(FALSE)
+#  published_zh     :boolean          default(FALSE)
+#  slug             :string(255)
+#  sold_total       :integer          default(0)
+#  updated_at       :datetime         not null
+#  width            :decimal(8, 2)
 #
 # Indexes
 #
