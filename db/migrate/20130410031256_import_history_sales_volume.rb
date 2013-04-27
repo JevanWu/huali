c64b9bee4869c53b4e652ba4f147e1c813694e45
Steven Yang <yangchenyun@gmail.com>
2013-04-27 15:07:46 +0800

    cleaning up tabs

--- db/migrate/20130410031256_import_history_sales_volume.rb
+++ db/migrate/20130410031256_import_history_sales_volume.rb
@@ -1,12 +1,12 @@
 class ImportHistorySalesVolume < ActiveRecord::Migration
   def up
-  	Order.where(state: "completed").each do |o|
-  	  o.line_items.each do |line|
-  	    p = line.product
-  	    p.sales_volume_totally += line.quantity
-  	    p.save
-  	  end
-  	end
+    Order.where(state: "completed").each do |o|
+      o.line_items.each do |line|
+        p = line.product
+        p.sales_volume_totally += line.quantity
+        p.save
+      end
+    end
   end
 
   def down
