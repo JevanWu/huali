3f7298f41252105ceb4baf54aa3403332f51629c
namiheike <namiheike@namiheike-ubuntu.(none)>
2013-05-06 18:10:32 +0800

    wrap kuaidi100 notiier into class

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -4,27 +4,39 @@ class ShipmentsController < ApplicationController
     File.open('kuaidi100_notify.out', 'a') { |file| file.write("query_string\n"+request.query_string+"\nraw_post"+request.raw_post+"\n") }
     render json: {result: 'true', returnCode: '200', message: '成功'}
 
-    identifier = CGI.parse(request.query_string)['identifier'].first
+    identifier = CGI.parse(query_string)['identifier'].first
     shipment = Shipment.where(identifier: identifier).first
-    post = JSON.parse URI.unescape(request.raw_post).sub('param=','')
-    lastResult = post['lastResult']
-    status = lastResult['state']
-    latestUpdatedTime = DateTime.parse(lastResult['data'][0]['ftime'])
 
-    return if latestUpdatedTime < shipment.kuaidi100_updated_at
+    kuaidi100_notifier = Kuaidi100Notifier.new(shipment, request.raw_post)
+    return if updated_time < shipment.kuaidi100_notifier.updated_time
+    kuaidi100_notifier.update_shipment
+  end
+end
 
-    shipment.kuaidi100_result = lastResult.to_json
-    shipment.kuaidi100_status = status
-    shipment.kuaidi100_updated_at = latestUpdatedTime
-    shipment.save
+class Kuaidi100Notifier
+  attr_reader :update_time, :status
 
-    case status
+  def initialize(@shipment, raw_post)
+    # parsing
+    post = JSON.parse URI.unescape(raw_post).sub('param=','')
+    @last_result = post['lastResult']
+    @status = last_result['state']
+    @updated_time = DateTime.parse(last_result['data'][0]['ftime'])
+  end
+
+  def update_shipment
+    @shipment.kuaidi100_result = @last_result.to_json
+    @shipment.kuaidi100_status = @status
+    @shipment.kuaidi100_updated_at = @updated_time
+    @shipment.save
+
+    case @status
     when 0,1
-      shipment.ship
+      @shipment.ship
     when 2
-      shipment.got_unknown_status
+      @shipment.got_unknown_status
     when 3
-      shipment.accept
+      @shipment.accept
     end
   end
 end
