b392c6daf8c7a7e71b6832e9d109a5fb31c9e7f1
Steven Yang <yangchenyun@gmail.com>
2013-05-07 17:42:48 +0800

    set no default value for datetime type of data

--- db/migrate/20130506025727_add_kuaidi100_updated_at_to_shipments.rb
+++ db/migrate/20130506025727_add_kuaidi100_updated_at_to_shipments.rb
@@ -1,5 +1,5 @@
 class AddKuaidi100UpdatedAtToShipments < ActiveRecord::Migration
   def change
-    add_column :shipments, :kuaidi100_updated_at, :datetime, :default => Time.at(0)
+    add_column :shipments, :kuaidi100_updated_at, :datetime
   end
 end
