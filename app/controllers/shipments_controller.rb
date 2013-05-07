7f66c9f45258115775a14bd46a27a119f4ec9549
Steven Yang <yangchenyun@gmail.com>
2013-05-07 17:45:42 +0800

    rewrite the shipment notifier listeners with openstruct

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,43 +1,37 @@
 # encoding: utf-8
+require 'ostruct'
+
 class ShipmentsController < ApplicationController
   def kuaidi100_notify
-    render json: {result: 'true', returnCode: '200', message: '成功'}
+    shipment = Shipment.find_by_identifier params['identifier']
+    notifier= ShipmentNotifier.new(params['param'])
 
-    identifier = CGI.parse(request.query_string)['identifier'].first
-    kuaidi100_notifier = Kuaidi100Notifier.new(identifier, request.raw_post)
-    kuaidi100_notifier.update_shipment if kuaidi100_notifier.need_update?
-  end
-end
+    if shipment.older_than_kuaidi100_notifier(notifier)
+      shipment.kuaidi100_result = notifier.data.to_json
+      shipment.kuaidi100_status = notifier.state
+      shipment.kuaidi100_updated_at = notifier.updated_time
 
-class Kuaidi100Notifier
-  attr_reader :update_time, :status, :need_update
-
-  def initialize(identifier, raw_post)
-    # parsing
-    @shipment = Shipment.find_by_identifier(identifier)
-    post = JSON.parse URI.unescape(raw_post).sub('param=','')
-    @last_result = post['lastResult']
-    @status = @last_result['state'].to_i
-    @updated_time = DateTime.parse(@last_result['data'][0]['ftime'])
-  end
-
-  def update_shipment
-    @shipment.kuaidi100_result = @last_result.to_json
-    @shipment.kuaidi100_status = @status
-    @shipment.kuaidi100_updated_at = @updated_time
-    @shipment.save
-
-    case @status
-    when 0,1
-      @shipment.ship
-    when 2
-      @shipment.got_unknown_status
-    when 3
-      @shipment.accept
+      if shipment.save!
+        render json: { result: 'true', returnCode: '200', message: '成功' }
+      end
     end
   end
 
-  def need_update?
-    @updated_time > @shipment.kuaidi100_updated_at
+end
+
+class ShipmentNotifier < OpenStruct
+  attr_reader :updated_time, :message, :status
+
+  def initialize(json_str)
+    raw_message = JSON.parse(json_str)
+    # this two messages might be useful
+    @message = raw_message['message']
+    @status = raw_message['status']
+
+    # important messages is in lastResult
+    super raw_message['lastResult']
+
+    # parse formated unified time format
+    @updated_time = DateTime.parse data.first['ftime']
   end
 end
