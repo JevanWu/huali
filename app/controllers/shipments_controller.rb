499f0a5d35a4a08dfcecd355e1f43eccb61ad86f
Steven Yang <yangchenyun@gmail.com>
2013-05-08 14:41:27 +0800

    the notify controller should always send a response

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -4,17 +4,17 @@ require 'ostruct'
 class ShipmentsController < ApplicationController
   def notify
     shipment = Shipment.find_by_identifier params['identifier']
-    notifier= ShipmentNotifier.new(params['param'])
+    notifier = ShipmentNotifier.new(params['param'])
 
     if shipment.older_than_kuaidi100_notifier(notifier)
       shipment.kuaidi100_result = notifier.data.to_json
       shipment.kuaidi100_status = notifier.state
       shipment.kuaidi100_updated_at = notifier.updated_time
 
-      if shipment.save!
-        render json: { result: 'true', returnCode: '200', message: '成功' }
-      end
+      shipment.save!
     end
+
+    render json: { result: 'true', returnCode: '200', message: '成功' }
   end
 end
 
