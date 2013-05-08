36f7c7c3984c11ad27dd66885363f0f72b8f013b
Steven Yang <yangchenyun@gmail.com>
2013-05-08 13:55:46 +0800

    use sidekiq to execute poll

--- app/models/shipment_observer.rb
+++ app/models/shipment_observer.rb
@@ -1,5 +1,5 @@
 class ShipmentObserver < ActiveRecord::Observer
   def after_accept(shipment, transition)
-    Shipment.kuaidi100_poll(shipment.id) if shipment.is_express?
+    Shipment.delay.kuaidi100_poll(shipment.id) if shipment.is_express?
   end
 end
