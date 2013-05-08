38e7902b9fbdd19bdbb05187cf13f104744c56ef
Steven Yang <yangchenyun@gmail.com>
2013-05-08 14:41:38 +0800

    hook the poll to ship instead of accept

--- app/models/shipment_observer.rb
+++ app/models/shipment_observer.rb
@@ -1,5 +1,5 @@
 class ShipmentObserver < ActiveRecord::Observer
-  def after_accept(shipment, transition)
+  def after_ship(shipment, transition)
     Shipment.delay.kuaidi100_poll(shipment.id) if shipment.is_express?
   end
 end
