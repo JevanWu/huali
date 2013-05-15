9e07cf4660b5ad08a81f8615895260a0f7ea6d14
namiheike <namiheike@namiheike-ubuntu.(none)>
2013-05-15 13:43:28 +0800

    fix :oauthable to :omniauthable, add :omniauth_providers in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :oauthable :registerable,
+  devise :database_authenticatable, :omniauthable, :omniauth_providers => [:weibo], :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me,
