9fb9460d1388cbab4089380fd6d8d7b093a354b8
namiheike <namiheike@gmail.com>
2013-05-22 11:17:53 +0800

    convert provider names into Chinese ones in views via I18N

--- app/controllers/oauth_services_controller.rb
+++ app/controllers/oauth_services_controller.rb
@@ -18,10 +18,10 @@ class OauthServicesController < Devise::OmniauthCallbacksController
   private
 
   def oauthorize(provider)
-    session[:oauth] = env["omniauth.auth"]
+    session[:oauth] = env['omniauth.auth']
     if @user = OauthService.find_user(provider, env['omniauth.auth']['uid'])
       # FIXME, 'douban' in this notice should be chinese one via i18n
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => provider
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: (I18n.t "devise.oauth_services.providers.#{provider}")
       sign_in_and_redirect @user, :event => :authentication
     else
       redirect_to new_oauth_user_registration_url
