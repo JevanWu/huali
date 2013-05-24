bf215929043f836f98567437e5e06fba3d36edbe
Steven Yang <yangchenyun@gmail.com>
2013-05-24 18:57:32 +0800

    add validations for oauth_service

--- app/models/oauth_service.rb
+++ app/models/oauth_service.rb
@@ -1,17 +1,15 @@
 class OauthService < ActiveRecord::Base
+
   attr_accessible :provider, :uid
 
   belongs_to :user
 
-  # FIXME this validator calls some errorr? 
-  # validates_presence_of :user
+  validates_presence_of :user
+  validates_presence_of :provider, :uid
 
   class << self
     def find_user(provider, uid)
-      if o = OauthService.where(provider: provider, uid: uid).first
-        o.user
-      end
+      OauthService.where(provider: provider, uid: uid).first
     end
   end
-
 end
