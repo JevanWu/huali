51d6a67e6bb4c4b93646e395e62fb1874dd14b92
Steven Yang <yangchenyun@gmail.com>
2013-05-31 18:35:32 +0800

    finished the recommendation algorithm

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -7,6 +7,10 @@ class SurveysController < ApplicationController
     @survey = Survey.new(params[:survey])
     @survey.user = current_user if current_user
 
+    @products = Product
+      .tagged_with([params[:survey][:receiver_gender]], any: true)
+      .tagged_with([params[:survey][:gift_purpose]], any: true)
+
     if @survey.save
       flash[:notice] = t('controllers.survey.survey_success')
     else
