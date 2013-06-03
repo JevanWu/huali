c1c2bed5eec2b0f5f34a07de423e7912ad7dadf1
Steven Yang <yangchenyun@gmail.com>
2013-06-03 13:58:44 +0800

    unscope Collection model in history migrations

--- db/migrate/20130311081547_add_primary_category_column_to_collections.rb
+++ db/migrate/20130311081547_add_primary_category_column_to_collections.rb
@@ -2,7 +2,7 @@ class AddPrimaryCategoryColumnToCollections < ActiveRecord::Migration
   def change
     add_column :collections, :primary_category, :boolean, null: false, default: false
     Collection.reset_column_information
-    Collection.all.each do |c|
+    Collection.unscoped.all.each do |c|
       c.primary_category = true
       c.save
     end
