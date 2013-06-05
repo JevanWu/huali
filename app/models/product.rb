5556dcc266e38a318adaa68d8f4a315f2de03a7c
ryancheung <ryancheung.go@gmail.com>
2013-06-05 18:13:21 +0800

    Fix the error logic and do a few refinement

--- app/models/product.rb
+++ app/models/product.rb
@@ -102,7 +102,7 @@ class Product < ActiveRecord::Base
   end
 
   def collection
-    collections.primary.blank? ? collections.primary.first : collections.first
+    collections.primary.blank? ? collections.first : collections.primary.first
   end
 
   def category_name
@@ -111,11 +111,11 @@ class Product < ActiveRecord::Base
   end
 
   def has_stock?
-    @count_on_hand
+    !!@count_on_hand
   end
 
   def discount?
-    !original_price.nil? && price < original_price
+    original_price.present? && price < original_price
   end
 
   def publish
