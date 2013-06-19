effa44f687a929cdcf0c813fcef6d2c491c868ad
Steven Yang <yangchenyun@gmail.com>
2013-06-19 16:19:09 +0800

    added type string to the order to distinguish orders from difference sources, updated the annotation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130607061559) do
+ActiveRecord::Schema.define(:version => 20130619081531) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -118,6 +118,11 @@ ActiveRecord::Schema.define(:version => 20130607061559) do
 
   add_index "collections_products", ["product_id", "collection_id"], :name => "index_collections_products_on_product_id_and_collection_id", :unique => true
 
+  create_table "collocation_relations", :force => true do |t|
+    t.integer "product_a_id", :null => false
+    t.integer "product_b_id", :null => false
+  end
+
   create_table "coupons", :force => true do |t|
     t.string   "code",                               :null => false
     t.string   "adjustment",                         :null => false
@@ -155,6 +160,74 @@ ActiveRecord::Schema.define(:version => 20130607061559) do
 
   add_index "oauth_services", ["provider", "uid"], :name => "index_oauth_services_on_provider_and_uid"
 
+  create_table "old_alipays", :force => true do |t|
+    t.string   "out_merchant_no"
+    t.string   "identifier"
+    t.string   "source"
+    t.string   "pay_type"
+    t.string   "customer"
+    t.string   "subject_text"
+    t.string   "amount"
+    t.string   "coupon"
+    t.string   "status"
+    t.string   "fee"
+    t.string   "refund"
+    t.string   "note"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  create_table "old_orders", :force => true do |t|
+    t.string   "order_status"
+    t.string   "order_number"
+    t.string   "buyer_name"
+    t.string   "phonenum"
+    t.string   "email"
+    t.string   "receiver_name"
+    t.string   "province"
+    t.string   "address"
+    t.date     "expect_date"
+    t.boolean  "need_invoice"
+    t.string   "invoice_header"
+    t.text     "requirement"
+    t.string   "receiver_phonenum"
+    t.string   "product_name"
+    t.string   "delivery_code"
+    t.text     "card_info"
+    t.string   "delivery_method"
+    t.string   "zip_code"
+    t.string   "comment"
+    t.boolean  "archived"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
+
+  create_table "old_wufoos", :force => true do |t|
+    t.string   "entry_id"
+    t.string   "status"
+    t.string   "order_identifier"
+    t.string   "name"
+    t.string   "phonenum"
+    t.string   "email"
+    t.string   "receiver_name"
+    t.string   "receiver_prov"
+    t.string   "receiver_addr"
+    t.string   "post_code"
+    t.string   "receiver_phonenum"
+    t.date     "expected_date"
+    t.text     "card_info"
+    t.text     "special_note"
+    t.string   "source"
+    t.string   "other_source"
+    t.string   "created_by"
+    t.string   "updated_by"
+    t.string   "ip_addr"
+    t.string   "last_access"
+    t.string   "completion_status"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
+
   create_table "order_coupons", :force => true do |t|
     t.integer "order_id"
     t.integer "coupon_id"
@@ -186,6 +259,7 @@ ActiveRecord::Schema.define(:version => 20130607061559) do
     t.string   "coupon_code"
     t.string   "ship_method_id"
     t.boolean  "printed",                                            :default => false
+    t.string   "type",                                               :default => ""
   end
 
   add_index "orders", ["identifier"], :name => "index_orders_on_identifier", :unique => true
--- spec/factories/surveys.rb
+++ spec/factories/surveys.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: surveys
+#
+#  created_at      :datetime         not null
+#  gender          :string(255)
+#  gift_purpose    :string(255)
+#  id              :integer          not null, primary key
+#  receiver_gender :string(255)
+#  updated_at      :datetime         not null
+#  user_id         :integer
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
