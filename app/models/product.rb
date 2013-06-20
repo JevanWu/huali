8ebe06e8803817d1cebaaf7c981114529e99261b
ryancheung <ryancheung.go@gmail.com>
2013-06-20 16:47:28 +0800

    Add presence validation to default_region_rule

--- app/models/product.rb
+++ app/models/product.rb
@@ -67,7 +67,7 @@ class Product < ActiveRecord::Base
   translate :name
 
   # validations
-  validates_presence_of :name_en, :name_zh, :count_on_hand, :assets, :collections
+  validates_presence_of :name_en, :name_zh, :count_on_hand, :assets, :collections, :default_region_rule
 
   # scopes
   default_scope lambda { order('priority DESC') }
