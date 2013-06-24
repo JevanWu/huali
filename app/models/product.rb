ee606be90ea038d4d21594cb645ab01c8b6f755b
Steven Yang <yangchenyun@gmail.com>
2013-06-24 12:06:44 +0800

    fix eager load bug on order edit

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,37 +2,39 @@
 #
 # Table name: products
 #
-#  count_on_hand    :integer          default(0), not null
-#  created_at       :datetime         not null
-#  depth            :decimal(8, 2)
-#  description      :text
-#  height           :decimal(8, 2)
-#  id               :integer          not null, primary key
-#  inspiration      :text
-#  meta_description :string(255)
-#  meta_keywords    :string(255)
-#  meta_title       :string(255)
-#  name_en          :string(255)      default(""), not null
-#  name_zh          :string(255)      default(""), not null
-#  original_price   :decimal(, )
-#  price            :decimal(8, 2)
-#  priority         :integer          default(5)
-#  published        :boolean          default(FALSE)
-#  slug             :string(255)
-#  sold_total       :integer          default(0)
-#  updated_at       :datetime         not null
-#  width            :decimal(8, 2)
+#  count_on_hand          :integer          default(0), not null
+#  created_at             :datetime         not null
+#  default_region_rule_id :integer
+#  depth                  :decimal(8, 2)
+#  description            :text
+#  height                 :decimal(8, 2)
+#  id                     :integer          not null, primary key
+#  inspiration            :text
+#  meta_description       :string(255)
+#  meta_keywords          :string(255)
+#  meta_title             :string(255)
+#  name_en                :string(255)      default(""), not null
+#  name_zh                :string(255)      default(""), not null
+#  original_price         :decimal(, )
+#  price                  :decimal(8, 2)
+#  priority               :integer          default(5)
+#  published              :boolean          default(FALSE)
+#  slug                   :string(255)
+#  sold_total             :integer          default(0)
+#  updated_at             :datetime         not null
+#  width                  :decimal(8, 2)
 #
 # Indexes
 #
-#  index_products_on_slug  (slug) UNIQUE
+#  index_products_on_default_region_rule_id  (default_region_rule_id)
+#  index_products_on_slug                    (slug) UNIQUE
 #
 
 
 class Product < ActiveRecord::Base
   attr_accessible :name_zh, :name_en, :description, :meta_title, :meta_description, :meta_keywords, :count_on_hand, :original_price, :price, :height, :width, :depth, :inspiration, :published, :priority
 
-  attr_accessible :tag_list, :recommendation_ids, :assets, :assets_attributes
+  attr_accessible :tag_list, :recommendation_ids, :assets, :assets_attributes, :date_rule_attributes, :local_region_rule_attributes, :default_region_rule_id
 
   # collection
   has_and_belongs_to_many :collections
@@ -51,11 +53,21 @@ class Product < ActiveRecord::Base
   has_many :recommendation_relations
   has_many :recommendations, :through => :recommendation_relations
 
+  # Area and Date rules
+  belongs_to :default_region_rule
+  has_one :local_region_rule, dependent: :destroy
+  accepts_nested_attributes_for :local_region_rule, allow_destroy: true, update_only: true,
+    reject_if: :all_blank
+
+  has_one :date_rule, dependent: :destroy
+  accepts_nested_attributes_for :date_rule, allow_destroy: true, update_only: true,
+    reject_if: proc { |d| d[:start_date].blank? || d[:end_date].blank? }
+
   # i18n translation
   translate :name
 
   # validations
-  validates_presence_of :name_en, :name_zh, :count_on_hand, :assets, :collections
+  validates_presence_of :name_en, :name_zh, :count_on_hand, :assets, :collections, :default_region_rule
 
   # scopes
   default_scope lambda { order('priority DESC') }
@@ -75,8 +87,7 @@ class Product < ActiveRecord::Base
     end
 
     def sort_by_collection
-      #FIXME remove try when assert products always have an associated collection
-      published.sort_by { |p| p.collection.try(:id) || 0 }
+      Product.published.joins(:collections).order(:collection_id)
     end
   end
 
@@ -138,5 +149,45 @@ class Product < ActiveRecord::Base
 
   def img(size)
     assets.first.image.url(size)
+  end
+
+  def available_provinces
+    possible_city_ids = region_rule.city_ids
+    Area.includes(:city).find_all_by_id(region_rule.area_ids).each do |area|
+      possible_city_ids << area.city.id unless possible_city_ids.include?(area.city.id)
+    end
+
+    available = Province.find_all_by_id(region_rule.province_ids)
+
+    City.includes(:province).find_all_by_id(possible_city_ids).each do |city|
+      available << city.province unless available.include?(city.province)
+    end
+
+    available
+  end
+
+  def available_cities_of_province(province_id)
+    available = Province.find(province_id).cities.find_all_by_id(region_rule.city_ids)
+
+    # Append cities of area
+    Area.where(parent_post_code: available.map(&:post_code)).find_all_by_id(region_rule.area_ids).map do |area|
+      available << area.city unless available.include?(area.city)
+    end
+
+    available
+  end
+
+  def available_areas_of_city(city_id)
+    City.find(city_id).areas.find_all_by_id(region_rule.area_ids)
+  end
+
+  def region_rule
+    self.local_region_rule || self.default_region_rule
+  end
+
+  def build_local_region_rule_upon_default
+    build_local_region_rule(province_ids: default_region_rule.province_ids,
+                            city_ids: default_region_rule.city_ids,
+                            area_ids: default_region_rule.area_ids)
   end
 end
