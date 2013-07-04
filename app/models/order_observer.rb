d364ca90f9067f46b77eca818c1367502e13d843
Steven Yang <yangchenyun@gmail.com>
2013-07-04 15:01:02 +0800

    updated order observers according to order types

--- app/models/order_observer.rb
+++ app/models/order_observer.rb
@@ -7,20 +7,25 @@ class OrderObserver < ActiveRecord::Observer
   end
 
   def after_pay(order, transition)
-    return unless order.type == 'normal'
+    return if order.type == 'marketing'
     Sms.delay.pay_order_user_sms(order.id)
     Notify.delay.pay_order_user_email(order.id)
+
+    # doesn't track taobao orders in GA
+    return if order.type == 'taobao'
     Notify.delay.pay_order_admin_email(order.id)
     AnalyticWorker.delay.complete_order(order.id)
     GaTrackWorker.delay.order_track(order.id)
   end
 
   def after_ship(order, transition)
+    return if order.type == "marketing"
     Notify.delay.ship_order_user_email(order.id)
     Sms.delay.ship_order_user_sms(order.id)
   end
 
   def after_confirm(order, transition)
+    return if order.type == "marketing"
     Sms.delay.confirm_order_user_sms(order.id)
   end
 end
