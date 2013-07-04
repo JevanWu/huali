9029cc3956d8b83fd429fb848ca2a1dd45e6a23d
ryancheung <ryancheung.go@gmail.com>
2013-07-04 13:31:17 +0800

    Add bypass virtual attribute to bypass regin/date validation

---
+++ app/assets/javascripts/admin_prov_city_area_update.js.coffee
@@ -0,0 +1,38 @@
+$ ->
+  [proSelector, citySelector, areaSelector] = $('select').filter (index) ->
+    $(@).attr('id').match /address_attributes/
+
+  updateProvinceSelector = ->
+    $.ajax
+      url: "/provinces"
+      dataType: 'json'
+      success: (data) -> $(proSelector).empty().append reduceToOptions(data)
+
+  updateCitySelector = (prov_id) ->
+    return unless prov_id?.length > 0
+    $.ajax
+      url: "/provinces/#{prov_id}/cities"
+      dataType: 'json'
+      success: (data) -> $(citySelector).empty().append reduceToOptions(data)
+
+  updateAreaSelector = (city_id) ->
+    return unless city_id?.length > 0
+    $.ajax
+      url: "/cities/#{city_id}/areas"
+      dataType: 'json'
+      success: (data) -> $(areaSelector).empty().append reduceToOptions(data)
+
+  emptyAreaSelector = ->
+    $(areaSelector).empty()
+
+  reduceToOptions = (data) ->
+    _.reduce(data, (memo, place) ->
+      memo + "<option value=#{place.id} data-postcode='#{place.post_code}'>#{place.name}</option>"
+    , '<option value>请选择</option>')
+
+  $(proSelector).on 'change',  ->
+    emptyAreaSelector()
+    updateCitySelector $(@).val()
+
+  $(citySelector).on 'change', ->
+    updateAreaSelector $(@).val()
--- app/models/order.rb
+++ app/models/order.rb
@@ -39,7 +39,8 @@ class Order < ActiveRecord::Base
 
   attr_accessible :line_items, :special_instructions, :address_attributes,
                   :gift_card_text, :delivery_date, :expected_date, :identifier, :state, :type,
-                  :sender_name, :sender_phone, :sender_email, :source, :adjustment, :coupon_code, :ship_method_id
+                  :sender_name, :sender_phone, :sender_email, :source, :adjustment, :coupon_code,
+                  :ship_method_id, :bypass_region_validation, :bypass_date_validation
   attr_accessor :bypass_region_validation, :bypass_date_validation
 
   belongs_to :address
@@ -73,9 +74,9 @@ class Order < ActiveRecord::Base
   validates_presence_of :identifier, :line_items, :expected_date, :state, :total, :item_total, :sender_email, :sender_phone, :sender_name
 
   validates_with OrderProductRegionValidator,
-    if: lambda { |order| order.not_yet_shipped? && !bypass_region_validation }
+    if: lambda { |order| order.not_yet_shipped? && !order.bypass_region_validation }
   validates_with OrderProductDateValidator,
-    if: lambda { |order| order.expected_date.present? && order.not_yet_shipped? && !bypass_date_validation }
+    if: lambda { |order| order.expected_date.present? && order.not_yet_shipped? && !order.bypass_date_validation }
 
   # only validate once on Date.today, because in future Date.today will change
   validate :phone_validate, unless: lambda { |order| order.sender_phone.blank? }
@@ -168,6 +169,14 @@ class Order < ActiveRecord::Base
     def full_info(key)
       includes(:user, :address, :transactions, :shipments).find_by_id(key)
     end
+  end
+
+  def bypass_region_validation=(value)
+    @bypass_region_validation = ActiveRecord::ConnectionAdapters::Column.value_to_boolean(value)
+  end
+
+  def bypass_date_validation=(value)
+    @bypass_date_validation = ActiveRecord::ConnectionAdapters::Column.value_to_boolean(value)
   end
 
   def not_yet_shipped?
