d4858a0dcc03bdba52b93625093c44c289253e35
ryancheung <ryancheung.go@gmail.com>
2013-07-05 17:03:30 +0800

    Refine settings, add UI to manage settings

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -1,18 +1,16 @@
 # encoding: utf-8
-ActiveAdmin.register_page "Global Settings" do
-  menu parent: "设置", label: -> { I18n.t("active_admin.global_settings") }, if: proc { authorized? :manage, Settings }
+ActiveAdmin.register Setting do
+  menu parent: "设置", if: proc { authorized? :manage, Setting }
+  actions :index, :edit, :show, :update, :new, :create
 
-  content :title => proc{ I18n.t("active_admin.global_settings")  } do
+  index do
+    column :id
+    column :var
+    column :thing_id
+    column :thing_type
+
+    default_actions
   end
 
-  controller do
-    def edit_product_rules
-      @product = OpenStruct.new(date_rule: OpenStruct.new, region_rule: OpenStruct.new)
-      @product.date_rule.start_at = nil
-      @product.date_rule.end_at = nil
-      @product.date_rule.included_dates = []
-      @product.date_rule.excluded_dates = []
-      @product.date_rule.excluded_weekdays = []
-    end
-  end
+  form partial: 'form'
 end
