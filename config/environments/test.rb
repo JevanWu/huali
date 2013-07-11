6c1757632cec1e7e9e1b56f1f492624ef73e1a32
Steven Yang <yangchenyun@gmail.com>
2013-07-11 20:10:57 +0800

    remove mass_assignment options in dev/test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,9 +32,6 @@ Huali::Application.configure do
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'example.com' }
 
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
-
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
