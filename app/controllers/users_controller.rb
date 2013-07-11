dbf5b74076eb4d6e872ff78072902707f394b30b
Steven Yang <yangchenyun@gmail.com>
2013-07-11 19:02:37 +0800

    enable strong parameters in checking user existence

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
+  respond_to :json
+
   def check_user_exist
-    if u = User.find_by_email(params[:user_email])
+    if u = User.find_by_email(check_user_params)
       result = { found: true, email: u.email }
     else
       result = { found: false }
     end
 
-    respond_to do |format|
-      format.json { render json: result }
-    end
+    respond_with(result)
+  end
+
+  private
+
+  def check_user_params
+    params.require(:user_email)
   end
 end
