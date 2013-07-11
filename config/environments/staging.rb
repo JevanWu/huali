97c427894587af1cb03314b8448efdf3dcf5baa2
Steven Yang <yangchenyun@gmail.com>
2013-07-11 20:12:50 +0800

    remove auto_explain configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,6 +3,7 @@ Huali::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
+  config.eager_load = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = true
@@ -70,10 +71,6 @@ Huali::Application.configure do
 
   # ImageMagick Process path on production server (Ubuntu 12.04 LTS)
   Paperclip.options[:command_path] = "/usr/bin/"
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Custome configurations
   # Log sms to file(Rails.root/tmp/sms/) instead really sending it
