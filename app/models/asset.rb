45732e00a00713a5919d9aa14cfb0b47f0c50f1e
Steven Yang <yangchenyun@gmail.com>
2013-07-11 18:34:43 +0800

    cleaning up strong parameters for assets

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -19,7 +19,6 @@
 
 class Asset < ActiveRecord::Base
   belongs_to :viewable, polymorphic: true
-  attr_accessible :image, :viewable_type
   validates_presence_of :image
   has_attached_file :image, styles: { medium: "310x300>", thumb: "93x90>"}
 end
