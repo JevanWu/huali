14e375b4b25a011f9dbe100598b89b6d1b0e94e3
Steven Yang <yangchenyun@gmail.com>
2013-07-11 21:01:44 +0800

    only require miniprofier when it is loaded

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,4 +1,4 @@
 # set FileStore in /tmp/miniprofiler
-if Rails.env == 'development'
+if Rails.env == 'development' && defined?(Rack::MiniProfiler)
   Rack::MiniProfiler.config.storage = Rack::MiniProfiler::FileStore
 end
