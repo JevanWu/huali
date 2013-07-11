7a92a19f0a24926496dd2e6dfd8a80610c962a5f
Steven Yang <yangchenyun@gmail.com>
2013-07-11 18:58:38 +0800

    cleaning up indirect assignmen attributes in tracking_cookie and order_coupon

--- app/models/order_coupon.rb
+++ app/models/order_coupon.rb
@@ -13,8 +13,6 @@
 #
 
 class OrderCoupon < ActiveRecord::Base
-  attr_accessible :coupon_id, :order_id
-
   belongs_to :order
   belongs_to :coupon
 end
--- app/models/tracking_cookie.rb
+++ app/models/tracking_cookie.rb
@@ -15,6 +15,5 @@
 
 class TrackingCookie < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :ga_client_id
   validates_presence_of :user, :ga_client_id
 end
