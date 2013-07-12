c69609381673ba2c9ea064b8155927ebbd961045
Steven Yang <yangchenyun@gmail.com>
2013-07-12 22:11:42 +0800

    doesn't use filters for settings, ransack will raise an issue

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -3,6 +3,8 @@ ActiveAdmin.register Setting do
   menu parent: "设置", if: proc { authorized? :manage, Setting }
   actions :index, :edit, :show, :update, :new, :create
 
+  config.filters = false
+  
   index do
     column :id
     column :var
