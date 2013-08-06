e351abac4c4287e75c4ce449ae64e3377db397a9
ryancheung <ryancheung.go@gmail.com>
2013-08-06 16:07:30 +0800

    Added making order and shipping order feature specs

---
+++ spec/features/making_order_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../spec_helper'
+
+feature "Making order" do
+  let(:admin) { create(:administrator, email: 'admin@example.com', password: 'adminx', password_confirmation: 'adminx') }
+  let(:ship_method) { create(:ship_method, name: 'USPS') }
+  given(:order) do
+    create(:order, :wait_check, ship_method: ship_method).tap do |o|
+      o.check
+    end
+  end
+
+  background do
+    login_as(admin, scope: :administrator)
+  end
+
+  scenario "Making successfully", js: true do
+    visit "/admin/orders/#{order.id}"
+    click_link '制作'
+    page.driver.browser.switch_to.alert.accept
+
+    page.should have_content('订单状态变更为等待发货')
+  end
+end
