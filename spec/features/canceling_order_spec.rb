db7f396f12466ee603d0c0e19aa60678c1f54188
ryancheung <ryancheung.go@gmail.com>
2013-08-06 17:22:03 +0800

    Added canceling order and completing order feature specs

---
+++ spec/features/canceling_order_spec.rb
@@ -0,0 +1,78 @@
+require_relative '../spec_helper'
+
+feature "Canceling order" do
+  let(:admin) { create(:administrator, email: 'admin@example.com', password: 'adminx', password_confirmation: 'adminx') }
+  let(:ship_method) { create(:ship_method, name: 'USPS') }
+
+  background do
+    login_as(admin, scope: :administrator)
+  end
+
+  scenario "order is waiting for payment", js: true do
+    order = create(:order)
+    visit "/admin/orders/#{order.id}"
+
+    click_link('取消')
+    page.driver.browser.switch_to.alert.accept
+
+    page.should have_content('订单状态变更为取消')
+    find("#order_#{order.id}").should have_content('取消')
+  end
+
+  scenario "order is waiting for auditing", js: true do
+    order = create(:order, :wait_check)
+    visit "/admin/orders/#{order.id}"
+
+    click_link('取消')
+    page.driver.browser.switch_to.alert.accept
+
+    page.should have_content('订单状态变更为取消')
+    find("#order_#{order.id}").should have_content('等待退款')
+  end
+
+  scenario "order is waiting for making", js: true do
+    order = create(:order, :wait_check, ship_method: ship_method).tap do |o|
+      o.check
+    end
+
+    visit "/admin/orders/#{order.id}"
+
+    click_link('取消')
+    page.driver.browser.switch_to.alert.accept
+
+    page.should have_content('订单状态变更为取消')
+    find("#order_#{order.id}").should have_content('等待退款')
+  end
+
+  scenario "order is waiting for shipping", js: true do
+    order = create(:order, :wait_check, ship_method: ship_method).tap do |o|
+      o.check
+      o.make
+    end
+
+    visit "/admin/orders/#{order.id}"
+
+    click_link('取消')
+    page.driver.browser.switch_to.alert.accept
+
+    page.should have_content('订单状态变更为取消')
+    find("#order_#{order.id}").should have_content('等待退款')
+  end
+
+  scenario "order was shipped", js: true do
+    order = create(:order,
+                   :wait_confirm,
+                   :with_one_transaction,
+                   :with_one_shipment)
+    create(:shipment, order: order, state: :shipped)
+
+    visit "/admin/orders/#{order.id}"
+
+    click_link('取消')
+    page.driver.browser.switch_to.alert.accept
+
+    page.should have_content('订单状态变更为取消')
+    find("#order_#{order.id}").should have_content('等待退款')
+  end
+end
+
---
+++ spec/features/completing_order_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../spec_helper'
+
+feature "Completing order" do
+  let(:admin) { create(:administrator, email: 'admin@example.com', password: 'adminx', password_confirmation: 'adminx') }
+
+  given(:order) do
+    o = create(:order,
+               :wait_confirm,
+               :with_one_transaction,
+               :with_one_shipment)
+    create(:shipment, order: o, state: :shipped)
+    o
+  end
+
+  background do
+    login_as(admin, scope: :administrator)
+  end
+
+  scenario "Complete successfully", js: true do
+    visit "/admin/orders/#{order.id}"
+
+    click_link('确认')
+    page.driver.browser.switch_to.alert.accept
+
+    page.should have_content('递送状态变更为已经送达')
+    find("#shipment_#{order.shipment.id}").should have_content('已经送达')
+  end
+end
