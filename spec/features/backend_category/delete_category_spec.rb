4e97f317c2d5929e09b03670c07ac3e10f1cf38a
ryancheung <ryancheung.go@gmail.com>
2013-08-07 17:20:56 +0800

    Complete backend category management feature specs

---
+++ spec/features/backend_category/delete_category_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature "Delete category" do
+  given(:super_admin) { create(:administrator, email: 'admin@example.com', password: 'adminx', password_confirmation: 'adminx', role: 'super') }
+
+  background do
+    login_as(super_admin, scope: :administrator)
+  end
+
+  scenario "Delete successfully", js: true do
+    category = create(:collection, name_en: 'wedding', name_zh: '婚礼', display_name: '婚礼')
+
+    visit "/admin/collections"
+    find("#collection_#{category.id}").click_link("删除")
+    page.driver.browser.switch_to.alert.accept
+
+    page.should_not have_content('婚礼')
+    page.should_not have_content('wedding')
+  end
+end
