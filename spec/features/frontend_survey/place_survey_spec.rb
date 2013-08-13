c5ddce0303114afefd735fde62ebdc1f22f7280b
ryancheung <ryancheung.go@gmail.com>
2013-08-13 11:23:58 +0800

    Added frontend survey feature spec

---
+++ spec/features/frontend_survey/place_survey_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+feature "Place survey" do
+  given(:product) { create(:product, name_en: 'ruby', name_zh: '红宝石') }
+
+  background do
+    # FIXME root page should always be setup up front
+    Page.create!(title_en: "Home", title_zh: '首页', permalink: 'home')
+    product.tag_list = '爱人'
+  end
+
+  scenario "Validation errors" do
+    visit "/surveys/new"
+    choose '男生'
+    choose '他'
+    click_button '帮我选花'
+
+    find('.survey_gift_purpose').should have_content('您需要填写此项')
+  end
+
+  scenario "Added successfully" do
+    visit "/surveys/new"
+    choose '男生'
+    choose '他'
+    choose '爱人'
+    click_button '帮我选花'
+
+    page.should have_content('感谢您的填写，下面是为您挑选的商品')
+    page.should have_link('红宝石')
+  end
+end
