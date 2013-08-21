ca876f4d2f8b9a47efea54519342da9a0b04be33
Steven Yang <yangchenyun@gmail.com>
2013-08-21 16:01:15 +0800

    finished the integration tests for OrderForm#save method

--- spec/models/order_form_integration_spec.rb
+++ spec/models/order_form_integration_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper_full'
 
 describe OrderForm do
 
-  before do
-    @product = FactoryGirl.create(:product)
+  before(:all) do
+    @product1 = FactoryGirl.create(:product)
+    @product2 = FactoryGirl.create(:product)
     @coupon = FactoryGirl.create(:coupon)
     @province = FactoryGirl.create(:province)
     @city = @province.cities.sample
@@ -30,8 +31,11 @@ describe OrderForm do
     }
   end
 
-  let(:valid_line_item) do
-    { product_id: @product.id, quantity: Forgery(:basic).number }
+  let(:valid_line_items) do
+    [
+      { product_id: @product1.id, quantity: Forgery(:basic).number },
+      { product_id: @product2.id, quantity: Forgery(:basic).number }
+    ]
   end
 
   let(:valid_order) do
@@ -39,7 +43,7 @@ describe OrderForm do
       # nested attributes
       sender: valid_sender,
       address: valid_receiver,
-      line_items: [valid_line_item],
+      line_items: valid_line_items,
       # direct attributes
       coupon_code: @coupon.code,
       gift_card_text: Forgery(:lorem_ipsum).paragraph,
@@ -50,12 +54,82 @@ describe OrderForm do
   end
 
   describe '#save' do
-    before do
-      OrderForm.new(valid_order).send(:persist!)
+    before(:all) do
+      @order_form = OrderForm.new(valid_order)
+      @order_form.save
     end
 
-    subject { Order.first }
+    let(:order) { Order.first }
 
-    it { subject.sender_email.should == 'somebody@example.com'}
+    context 'sender information' do
+      subject { order }
+
+      it 'stores sender_email' do
+        subject.sender_email.should == valid_sender[:email]
+      end
+
+      it 'stores sender_name' do
+        subject.sender_name.should == valid_sender[:name]
+      end
+
+      it 'stores sender_phone' do
+        subject.sender_phone.should == valid_sender[:phone]
+      end
+    end
+
+    context 'address information' do
+      subject { order.address }
+
+      it { should_not be_nil }
+
+      it 'saves province_id' do
+        subject.province_id.should == valid_receiver[:province_id]
+      end
+
+      it 'saves city_id' do
+        subject.city_id.should == valid_receiver[:city_id]
+      end
+
+      it 'saves area_id' do
+        subject.area_id.should == valid_receiver[:area_id]
+      end
+
+      it 'saves receiver fullname' do
+        subject.fullname.should == valid_receiver[:fullname]
+      end
+
+      it 'saves phone number' do
+        subject.phone.should == valid_receiver[:phone]
+      end
+
+      it 'saves detailed address' do
+        subject.address.should == valid_receiver[:address]
+      end
+
+      it 'saves post_code' do
+        subject.post_code.should == valid_receiver[:post_code]
+      end
+    end
+
+    context 'line_items information' do
+      subject { order.line_items }
+
+      it 'saves the each product' do
+        subject[0].product == @product1
+        subject[1].product == @product2
+      end
+
+      it 'saves quantity for each product' do
+        subject[0].quantity == valid_line_items[0][:quantity]
+        subject[1].quantity == valid_line_items[1][:quantity]
+      end
+    end
+
+    context 'other information' do
+      subject { order }
+      it { subject.coupon_code.should == @coupon.code }
+      it { subject.gift_card_text.should == valid_order[:gift_card_text] }
+      it { subject.special_instructions.should == valid_order[:special_instructions] }
+    end
   end
 end
