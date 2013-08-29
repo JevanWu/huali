8ad86a8a0c3e0404d90894b52643f378418acebe
Steven Yang <yangchenyun@gmail.com>
2013-08-29 16:28:14 +0800

    extract the prefix phone string manipulate methods

--- lib/phonelib_extension.rb
+++ lib/phonelib_extension.rb
@@ -9,10 +9,10 @@ module Phonelib
         if phone_calling_code == "+86"
           sanitized = Phonelib.parse(original_phone).sanitized.sub(/^86/, '')
 
-          phonelib_fixed_line(sanitized)
+          prefix_local_fixed_number(sanitized)
         else
           phone_to_parse = "#{phone_calling_code} #{original_phone}"
-          "+#{Phonelib.parse(phone_to_parse).sanitized}"
+          prefix(:+, Phonelib.parse(phone_to_parse).sanitized)
         end
       end
 
@@ -22,19 +22,20 @@ module Phonelib
         if sanitized.start_with?('86')
           sanitized.sub!(/^86/, '')
 
-          phonelib_fixed_line(sanitized)
+          prefix_local_fixed_number(sanitized)
         else
-          "+#{sanitized}"
+          prefix(:+, sanitized)
         end
       end
 
-      def phonelib_fixed_line(phone)
+      def prefix_local_fixed_number(phone)
         # Chinese fixed-line number needs to be prefixed with '0'
-        if phone.start_with?('400') || (phone.start_with?('1') && !phone.start_with?('10'))
-          phone
-        else
-          "0#{phone}"
-        end
+        # 400-* and cellphone numbers will be escaped
+        phone.match(/^(400|1[^0])/) ? phone : prefix('0', phone)
+      end
+
+      def prefix(sign, number)
+        sign.to_s + number.to_s
       end
     end
 
