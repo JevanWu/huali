f1fa6288f88ca8898b4fe937bb496e787d071eaa
ryancheung <ryancheung.go@gmail.com>
2013-08-30 11:15:32 +0800

    Split OrderItemValidator into two: OrderItemValidator and OrderProductAvailabilityValidator

--- app/models/order_item_validator.rb
+++ app/models/order_item_validator.rb
@@ -7,29 +7,16 @@ class OrderItemValidator < ActiveModel::Validator
       return
     end
 
-    order_valid = true
-
     order.line_items.each do |line_item|
-      order_valid = false unless validate_line_item(line_item)
+      validate_line_item(line_item)
     end
-
-    order.errors.add(:base, :unavailable_products) unless order_valid
   end
 
   def validate_line_item(line_item)
-    item_valid = true
     product = fetch_product(line_item)
 
-    unless product.published
-      line_item.errors.add(:base, :product_unavailable, product_name: product.name)
-      item_valid = false
+    unless line_item.quantity > 0
+      line_item.errors.add(:base, :product_of_invalid_quantity, product_name: product.name)
     end
-
-    unless sufficient_stock?(line_item)
-      line_item.errors.add(:base, :product_out_of_stock, product_name: product.name)
-      item_valid = false
-    end
-
-    item_valid
   end
 end
---
+++ app/models/order_product_availability_validator.rb
@@ -0,0 +1,31 @@
+class OrderProductAvailabilityValidator < ActiveModel::Validator
+  include OrderProductValidationHelper
+
+  def validate(order)
+    order_valid = true
+
+    order.line_items.each do |line_item|
+      order_valid = false unless validate_line_item(line_item)
+    end
+
+    order.errors.add(:base, :unavailable_products) unless order_valid
+  end
+
+  def validate_line_item(line_item)
+    item_valid = true
+    product = fetch_product(line_item)
+
+    unless product.published
+      line_item.errors.add(:base, :product_unavailable, product_name: product.name)
+      item_valid = false
+    end
+
+    unless sufficient_stock?(line_item)
+      line_item.errors.add(:base, :product_out_of_stock, product_name: product.name)
+      item_valid = false
+    end
+
+    item_valid
+  end
+end
+
