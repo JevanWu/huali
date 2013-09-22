c46be71e09afabf14eec095445be171cba618241
ryancheung <ryancheung.go@gmail.com>
2013-09-22 10:47:45 +0800

    Added spec for model Banner

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,5 +1,5 @@
 class Banner < ActiveRecord::Base
   def self.fetch_by_date(date)
-    where("start_date >= ? and end_date <= ?", date, date)
+    where("start_date <= ? AND end_date >= ?", date, date)
   end
 end
--- spec/models/banner_spec.rb
+++ spec/models/banner_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Banner do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".fetch_by_date" do
+    before do
+      @banner = create(:banner)
+    end
+
+    subject { described_class.fetch_by_date("2013-09-19").to_a }
+
+    it "returns the banners available in the date" do
+      subject.should include(@banner)
+    end
+  end
 end
