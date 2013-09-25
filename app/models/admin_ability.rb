0b6a08430a92af86eb01a97a4ef4a427d0e61040
ryancheung <ryancheung.go@gmail.com>
2013-09-25 18:11:18 +0800

    Added cancan abilities for Administrator

--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -4,6 +4,8 @@ class AdminAbility
   def initialize(user)
     user ||= Administrator.new
 
+    alias_action :update_product_seo, :update_collection_seo, :to => :update_seo
+
     case user.role
     when "super"
       can :manage, :all
@@ -12,12 +14,23 @@ class AdminAbility
       cannot :manage, Administrator
       cannot :manage, Sidekiq
       cannot :destroy, :all
-    when "supplier"
-      can :read, Order, state: "wait_ship"
-      #cannot :read, Order
-      #can :read, Order, state: 'wait_ship'
-      #can :manage, Shipment
-      #cannot :delete, Shipment
+    when "operation_manager"
+      can :manage, [Product, Collection, Order, Transaction, Shipment, Coupon, DefaultRegionRule, DefaultDateRule]
+      cannot :bulk_export_data
+      cannot :destroy, :all
+      cannot :update_seo
+    when "product_manager"
+      can :manage, [Product, Collection, Asset]
+      cannot :destroy, :all
+      cannot :update_seo
+    when "web_operation_manager"
+      can :manage, [Page, Product, Collection, Coupon, Asset, Setting]
+      cannot :destroy, :all
+      can :update_seo
+    when "marketing_manager"
+      can :manage, [Coupon]
+      can :record_back_order
+      cannot :destroy, :all
     end
   end
 end
