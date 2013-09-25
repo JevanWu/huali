b4b61a75fcb9c176e42dbe3806304a5e9a721f43
ryancheung <ryancheung.go@gmail.com>
2013-09-25 10:51:14 +0800

    Remove useless code

--- config/application.rb
+++ config/application.rb
@@ -20,14 +20,6 @@ module Huali
 
     end
 
-    config.to_prepare do
-      #Devise::SessionsController.layout 'application_simple'
-      #Devise::RegistrationsController.layout 'application'
-      #Devise::ConfirmationsController.layout "application_simple"
-      #Devise::UnlocksController.layout "application_simple"
-      #Devise::PasswordsController.layout "application_simple"
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
