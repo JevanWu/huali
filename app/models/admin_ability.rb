80bd605a8bff693c9d96105ba9dd25cdc21fd2f1
ryancheung <ryancheung.go@gmail.com>
2013-09-26 14:48:20 +0800

    Refactor cancan AdminAbility

--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -2,7 +2,13 @@ class AdminAbility
   include CanCan::Ability
 
   def initialize(user)
-    user ||= Administrator.new
+    user ||= Administrator.new(role: '')
+
+    if user.persisted?
+      can :read, ActiveAdmin::Page, :name => "Dashboard"
+      can :read, Order
+      cannot :destroy, :all unless user.role == "super"
+    end
 
     case user.role
     when "super"
@@ -11,27 +17,20 @@ class AdminAbility
       can :manage, :all
       cannot :manage, Administrator
       cannot :manage, Sidekiq
-      cannot :destroy, :all
     when "operation_manager"
       can :manage, [Product, Collection, Order, Transaction, Shipment, Coupon, DefaultRegionRule, DefaultDateRule]
       cannot :bulk_export_data, [Product, Order, Transaction, Shipment]
-      cannot :destroy, :all
       cannot :update_seo, [Product, Collection]
+      can :record_back_order, Order
     when "product_manager"
       can :manage, [Product, Collection, Asset]
-      can :read, Order
-      cannot :destroy, :all
       cannot :update_seo, [Product, Collection]
     when "web_operation_manager"
       can :manage, [Page, Product, Collection, Coupon, Asset, Setting]
-      can :read, Order
-      cannot :destroy, :all
       can :update_seo, [Product, Collection]
     when "marketing_manager"
       can :manage, [Coupon]
-      can :read, Order
-      can :record_back_order
-      cannot :destroy, :all
+      can :record_back_order, Order
     end
   end
 end
