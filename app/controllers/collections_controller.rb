b912ac25b2bfc314f9f0abc5ffffd46e71ee235d
ryancheung <ryancheung.go@gmail.com>
2013-10-10 15:36:45 +0800

    Display products in child collections of a top collection

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,7 +2,9 @@ class CollectionsController < ApplicationController
   def show
     @collection = Collection.available.find(params[:id])
 
-    @products = @collection.products.published.page(params[:page])
+    @products = Product.published.joins(:collections).
+      where("collections_products.collection_id in (?)",
+            @collection.self_and_descendants.map(&:id)).page(params[:page])
 
     respond_to do |format|
       format.html { render 'show'}
