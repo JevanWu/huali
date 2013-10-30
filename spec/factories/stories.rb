20ed3c5ad22f768fdf82d7e90a363132ccfe0a80
ryancheung <ryancheung.go@gmail.com>
2013-10-30 11:42:45 +0800

    Remove blank files

--- spec/factories/stories.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :story do
-    name "MyString"
-    description "MyString"
-    available false
-  end
-end
