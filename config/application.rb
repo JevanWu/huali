2ede406109bee88a100f032dbe506770c5867524
ryancheung <ryancheung.go@gmail.com>
2013-10-31 18:50:44 +0800

    Added lib/utils.rb to eager load path to solve sidekiq exception

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module Huali
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/workers #{config.root}/app/values #{config.root}/app/services)
 
+    config.eager_load_paths += %W(#{config.root}/lib)
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
