2278145132ceb21ba6a89fa5c9f5758620531f85
ryancheung <ryancheung.go@gmail.com>
2013-11-05 12:33:38 +0800

    Added paginator for orders in dashboard page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -9,12 +9,15 @@ ActiveAdmin.register_page "Dashboard" do
     columns do
       column do
         panel "最近进来的订单" do
-          table_for Order.order('id desc').limit(10) do
-            column("状态")  { |order| status_tag t('models.order.state.' + order.state), order_state(order) }
-            column("订单号")  { |order| link_to(order.identifier, admin_order_path(order)) }
-            column("产品")  { |order| order.subject_text }
-            column("金额")  { |order| number_to_currency order.item_total }
-            column("下单时间")  { |order| l order.created_at, format: :short }
+          orders = Order.order("created_at DESC")
+          paginated_collection(orders.page(params[:orders_page]).per(10), param_name: 'orders_page') do
+            table_for collection do |order|
+              column("状态")  { |order| status_tag t('models.order.state.' + order.state), order_state(order) }
+              column("订单号")  { |order| link_to(order.identifier, admin_order_path(order)) }
+              column("产品")  { |order| order.subject_text }
+              column("金额")  { |order| number_to_currency order.item_total }
+              column("下单时间")  { |order| l order.created_at, format: :short }
+            end
           end
         end
       end
