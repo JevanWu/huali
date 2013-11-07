ebb51ccc2e3150df69f8497ecc0a84006a868774
ryancheung <ryancheung.go@gmail.com>
2013-11-07 15:04:15 +0800

    Updated model Survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -18,8 +18,8 @@ class Survey < ActiveRecord::Base
   enumerize :gender, in: [:male, :female]
   enumerize :receiver_gender, in: [:male, :female]
   enumerize :receiver_age, in: [:eighteen_and_bellow, :nineteen_to_25, :twenty_six_to_40, :forty_one_to_60, :sixty_above]
-  enumerize :relationship, in: [:girlfriend, :boyfriend, :wife, :husband, :colleague, :friend, :teacher, :relative, :children, :other]
-  enumerize :gift_purpose, in: [:new_born, :propose, :marriage, :birthday_and_anniversary, :apology, :confession, :business, :wish, :other]
+  enumerize :relationship, in: [:girlfriend, :boyfriend, :wife, :husband, :colleague, :friend, :teacher, :relative, :children, :other_relationship]
+  enumerize :gift_purpose, in: [:new_born, :propose, :marriage, :birthday_and_anniversary, :apology, :confession, :business, :wish, :other_purpose]
 
   validates_presence_of :gender, :receiver_gender, :receiver_age, :relationship, :gift_purpose
 end
