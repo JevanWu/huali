1df6916c680de2e39e272a691a7a7da68f11a890
ryancheung <ryancheung.go@gmail.com>
2013-11-19 17:44:47 +0800

    Updated url path

--- config/routes.rb
+++ config/routes.rb
@@ -74,7 +74,7 @@ Huali::Application.routes.draw do
 
   get 'banners/:date', to: 'banners#index', as: :banners
   get 'stories', to: 'stories#index'
-  resources :didi_passengers, only: [:new, :create]
+  resources :didi_passengers, only: [:new, :create], path: 'didi_taxi'
 
   ActiveAdmin.routes(self)
 
