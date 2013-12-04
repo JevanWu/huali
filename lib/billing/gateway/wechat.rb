b628abce74bb25398703ebc186949d1f52d02cd1
ryancheung <ryancheung.go@gmail.com>
2013-12-04 19:09:06 +0800

    Added wechat payment gateway class

---
+++ lib/billing/gateway/wechat.rb
@@ -0,0 +1,50 @@
+require 'digest/md5'
+
+module Billing
+  class Gateway
+    class Wechat < Gateway::Base
+
+      SERVICE_URL = "https://gw.tenpay.com/gateway/pay.htm?"
+
+      def initialize(opts, query = nil)
+        super
+      end
+
+      def to_s
+        query = add_sign(query_string)
+        SERVICE_URL + URI.encode(query)
+      end
+
+      private
+
+      def default_opts
+        {
+          # gateway related
+          partner: ENV['WECHAT_PID'],
+          input_charset: "UTF-8",
+
+          # default options
+          fee_type: "1"
+          return_url: return_order_url(host: $host || 'localhost') + custom_data,
+          notify_url: notify_order_url(host: $host || 'localhost') + custom_data,
+
+          # shared options
+          body: @opts[:body],
+          out_trade_no: @opts[:identifier],
+          total_fee: @opts[:amount],
+          spbill_create_ip: @opts[:client_ip]
+        }
+      end
+
+      def to_options(opts)
+        {}
+      end
+
+      def add_sign(query)
+        sign = Digest::MD5.hexdigest(query + ENV['ALIPAY_KEY'])
+        query += "&sign=#{sign}&sign_type=MD5"
+      end
+    end
+  end
+end
+
