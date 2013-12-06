b066d70e854a2a1c78e70e280c91745df20b329e
ryancheung <ryancheung.go@gmail.com>
2013-12-06 11:27:20 +0800

    Updated wechat gateway parameters

--- lib/billing/gateway/wechat.rb
+++ lib/billing/gateway/wechat.rb
@@ -25,6 +25,7 @@ module Billing
 
           # default options
           fee_type: "1",
+          bank_type: "WX",
           return_url: return_order_url(host: $host || 'localhost') + custom_data,
           notify_url: notify_order_url(host: $host || 'localhost') + custom_data,
 
