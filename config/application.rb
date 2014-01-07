2004b5be9bf4e393293863c36def7f1442e2a6bf
ryancheung <ryancheung.go@gmail.com>
2014-01-07 16:53:48 +0800

    Remove needless config

--- config/application.rb
+++ config/application.rb
@@ -30,10 +30,6 @@ module Huali
 
     config.eager_load_paths += %W(#{config.root}/lib)
 
-    # APIs
-    config.paths.add "lib/api", glob: "**/*.rb"
-    config.autoload_paths += Dir["#{Rails.root}/lib/api/*"]
-
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
