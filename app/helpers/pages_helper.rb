6ca43a2fec43420dd57765f96210debe8778d7cd
ryancheung <ryancheung.go@gmail.com>
2014-01-22 21:42:23 +0800

    Considering the situation that no product promo is created

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def path_to_promo_product
     promo_today = LimitedPromotion.start_today.first || LimitedPromotion.first
-    product_path(promo_today.product)
+    product_path(promo_today.product) if promo_today
   end
 end
