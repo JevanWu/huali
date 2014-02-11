50414b812fc19eb8d58fdc91a562739373e8327c
ryancheung <ryancheung.go@gmail.com>
2014-02-11 15:58:52 +0800

    Try to fix ApiAgentWorder

--- app/services/api_agent_service.rb
+++ app/services/api_agent_service.rb
@@ -1,18 +1,20 @@
 class ApiAgentService
   class << self
     def update_receiver_address(order)
-      if ["wait_check", "wait_make", "wait_ship"].include?(order.state)
-        if address_just_modified?(order)
-          options = {
-            fullname: order.address.fullname,
-            phone: order.address.phone,
-            province: order.address.province_name,
-            city: order.address.city_name,
-            area: order.address.area_name,
-            address: order.address.address,
-            post_code: order.address.post_code
-          }
-          ApiAgentWorker.delay.update_receiver_address(order.kind, order.merchant_order_no, options)
+      if ["taobao", "tmall"].include?(order.kind)
+        if ["wait_check", "wait_make", "wait_ship"].include?(order.state)
+          if address_just_modified?(order)
+            options = {
+              fullname: order.address.fullname,
+              phone: order.address.phone,
+              province: order.address.province_name,
+              city: order.address.city_name,
+              area: order.address.area_name,
+              address: order.address.address,
+              post_code: order.address.post_code
+            }
+            ApiAgentWorker.delay.update_receiver_address(order.kind.to_s, order.merchant_order_no, options)
+          end
         end
       end
     end
@@ -26,7 +28,7 @@ class ApiAgentService
     def ship_order(order)
       if ["taobao", "tmall"].include?(order.kind)
         shipment = order.shipment
-        ApiAgentWorker.delay.ship_order(order.kind,
+        ApiAgentWorker.delay.ship_order(order.kind.to_s,
                                         order.merchant_order_no,
                                         shipment.ship_method_id,
                                         shipment.tracking_num)
