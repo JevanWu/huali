6fcc2d9b376f20aa3e9372e795ab179ceecf07ba
jevanwu <jevanwu@gmail.com>
2014-02-19 15:47:45 +0800

    [feature] customise the view of 'index' and 'show' of slide_panel in ActiveAdmin

--- app/admin/slide_panel.rb
+++ app/admin/slide_panel.rb
@@ -17,4 +17,43 @@ ActiveAdmin.register SlidePanel do
       f.actions
     end
   end
+
+  index do
+    selectable_column
+    column "ID" do |slide_panel|
+      link_to slide_panel.id, admin_slide_panel_path(slide_panel)
+    end
+    column :name
+    column :href
+    column :priority
+    column :image do |slide_panel|
+      image_tag slide_panel.img(:thumb)
+    end
+    default_actions
+  end
+
+
+  show do
+    attributes_table do
+      row :name
+      row :href
+      row :priority
+      row :created_at
+      row :updated_at
+      row :file_name do
+        slide_panel.image_file_name
+      end
+      row :file_size do
+        number_to_human_size(slide_panel.image_file_size)
+      end
+      row :thumbnails do
+        image_tag slide_panel.image.url(:thumb)
+      end
+
+      row :medium_image do
+        image_tag slide_panel.image.url(:medium)
+      end
+      row :image_updated_at
+    end
+  end
 end
