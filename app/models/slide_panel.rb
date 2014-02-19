64788adb0cf54f20b8a2be4f637fde4b7d3cad44
jevanwu <jevanwu@gmail.com>
2014-02-19 17:44:51 +0800

    [chore] delete the needless code

--- app/models/slide_panel.rb
+++ app/models/slide_panel.rb
@@ -1,6 +1,5 @@
 class SlidePanel < ActiveRecord::Base
   has_many :assets, as: :viewable, dependent: :destroy
-  accepts_nested_attributes_for :assets, reject_if: lambda { |a| a[:image].blank? }, allow_destroy: true
 
   validates :priority, presence: true, uniqueness: true, numericality: {only_integer: true, greater_than: 0}
   
