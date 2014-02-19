b0b881fe786d757ef717403b67b286642e19ac11
jevanwu <jevanwu@gmail.com>
2014-02-19 15:46:10 +0800

    [feature] add 'img' method to slide_panel model

--- app/models/slide_panel.rb
+++ app/models/slide_panel.rb
@@ -3,4 +3,10 @@ class SlidePanel < ActiveRecord::Base
   accepts_nested_attributes_for :assets, reject_if: lambda { |a| a[:image].blank? }, allow_destroy: true
 
   validates :priority, presence: true, uniqueness: true, numericality: {only_integer: true, greater_than: 0}
+  
+  has_attached_file :image, styles: { medium: "1583x594>", small: "408x153>", thumb: "266x100>" }
+
+  def img(size)
+    self.image.url(size)
+  end
 end
