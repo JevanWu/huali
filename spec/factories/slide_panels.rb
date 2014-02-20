dbb9b14d31b0b47727dc7674a26173ab043070a2
jevanwu <jevanwu@gmail.com>
2014-02-20 17:28:30 +0800

    [chore] prepare test data for feature test

--- spec/factories/slide_panels.rb
+++ spec/factories/slide_panels.rb
@@ -1,9 +1,8 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :slide_panel do
-    name "MyString"
-    href "MyString"
+    name { Forgery(:lorem_ipsum).word }
+    href "/"
     priority 1
     visible false
   end
