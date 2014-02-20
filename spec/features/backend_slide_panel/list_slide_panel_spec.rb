e8251cef2f990247d5b7cb5a3c8cae6f6557ac9c
jevanwu <jevanwu@gmail.com>
2014-02-20 17:38:06 +0800

    [test] feature test of backend 'list slide panels'

---
+++ spec/features/backend_slide_panel/list_slide_panel_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature "list slide panel" do
+  let(:admin) { create(:administrator) }
+
+  background do
+    login_as(admin, scope: :administrator)
+    create(:slide_panel, name: 'valentine', priority: 1)
+    create(:slide_panel, name: 'hualigirls', priority: 2)
+  end
+
+  scenario "list all slides" do
+    visit "/admin/slide_panels"
+
+    page.should have_content('valentine')
+    page.should have_content('hualigirls')
+  end
+end
