db0d9f7447c198b368af19658eb12966b8427235
ryancheung <ryancheung.go@gmail.com>
2014-03-03 11:50:33 +0800

    Push API exceptions to squash

--- lib/api/api.rb
+++ lib/api/api.rb
@@ -13,15 +13,8 @@ module API
     end
 
     rescue_from :all do |exception|
-      # lifted from https://github.com/rails/rails/blob/master/actionpack/lib/action_dispatch/middleware/debug_exceptions.rb#L60
-      # why is this not wrapped in something reusable?
-      trace = exception.backtrace
+      Squash::Ruby.notify exception
 
-      message = "\n#{exception.class} (#{exception.message}):\n"
-      message << exception.annoted_source_code.to_s if exception.respond_to?(:annoted_source_code)
-      message << "  " << trace.join("\n  ")
-
-      API.logger.add Logger::FATAL, message
       rack_response({'message' => '500 Internal Server Error'}.to_json, 500)
     end
 
