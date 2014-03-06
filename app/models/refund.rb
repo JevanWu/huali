e37503925270f5a2a527433bf38bbc795df7e7be
ryancheung <ryancheung.go@gmail.com>
2014-03-06 15:50:12 +0800

    Fixed relation, validations, added callback to set state

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -1,8 +1,18 @@
 class Refund < ActiveRecord::Base
   belongs_to :order
+  belongs_to :transaction
 
   validates :merchant_refund_id, uniqueness: { scope: :order_id }, allow_nil: true
   validates :transaction, :order, :amount, presence: true
+
+  validates :ship_method, presence: true, if: Proc.new { |refund| refund.tracking_number? }
+  validates :tracking_number, presence: true, if: Proc.new { |refund| refund.ship_method? }
+
+  after_save do |refund|
+    if refund.state == 'pending' && refund.ship_method? && refund.tracking_number?
+      refund.return_goods
+    end
+  end
 
   state_machine :state, initial: :pending do
     state :pending do
