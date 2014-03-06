ee86db12a03d30f3729ab501c1a8023a8ac650d7
ryancheung <ryancheung.go@gmail.com>
2014-03-06 14:16:28 +0800

    Added state machine to Refund

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -1,3 +1,22 @@
 class Refund < ActiveRecord::Base
   belongs_to :order
+
+  validates :merchant_refund_id, uniqueness: { scope: :order_id }, allow_nil: true
+  validates :transaction, :order, :amount, presence: true
+
+  state_machine :state, initial: :pending do
+    state :pending do
+      transition to: :accepted, on: :accept
+      transition to: :rejected, on: :reject
+
+      transition to: :goods_returned, on: :return_goods
+    end
+
+    state :goods_returned do
+      validates :ship_method, :tracking_number, presence: true
+
+      transition to: :accepted, on: :accept
+      transition to: :rejected, on: :reject
+    end
+  end
 end
