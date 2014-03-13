8a37a5092d1e3db28976cd12df41122650c5c4f3
jevanwu <jevanwu@gmail.com>
2014-03-13 18:37:17 +0800

    [feature] service of that use huali points to buy product

--- app/services/huali_point_service.rb
+++ app/services/huali_point_service.rb
@@ -21,14 +21,27 @@ class HualiPointService
   end
 
   def self.rebate_point(customer, transaction)
-    if transaction.point_transaction == nil
+    unless transaction.point_transaction
       User.transaction do
         customer.lock!
         customer.edit_huali_point(transaction.amount*0.01)
-        customer.create_income_point_transaction(transaction.amount*0.01, transaction_id: transaction.id)
+        customer.create_income_point_transaction(transaction.amount*0.01, transaction.id)
         customer.save!
       end
     end
   end
 
+  def self.minus_expense_point(customer, transaction)
+    if transaction.use_huali_point?
+      User.transaction do 
+        if customer.huali_point > transaction.order.total.to_i
+          customer.edit_huali_point(-transaction.order.total.to_i)
+          customer.create_expense_point_transaction(transaction.order.total.to_i, transaction.id)
+        else
+          customer.edit_huali_point(-customer.huali_point)
+          customer.create_expense_point_transaction(customer.huali_point, transaction.id)
+        end
+      end
+    end
+  end
 end
