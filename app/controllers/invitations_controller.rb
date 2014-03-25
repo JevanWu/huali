cbd8ffff31e9f7b4a85d51e2aa41008c3bddc155
jevanwu <jevanwu@gmail.com>
2014-03-25 14:54:56 +0800

    [feature] modify the mailer

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,22 +1,16 @@
 class InvitationsController < Devise::InvitationsController
   def create
-    @from = current_user.email
-    @subject = params[:invite_subject]
-    @content = params[:invite_content]
+    @from = "#{current_user.name.titleize} via Huali"
+    @subject = "#{current_user.name.titleize} invited you to checkout Huali"
     @user = User.invite!(params[:user], current_user) do |u|
       u.skip_invitation = true
     end
 
-    if !@subject.present? || !@content.present?
-      @user.errors[:base] = t("devise.invitations.new.subject_content_not_be_empty")
-      render :new and return
-    end
-    
     if @user.invitation_sent_at
       @user.errors[:base] = t("devise.invitations.new.has_been_our_user")
     else
       @user.deliver_invitation
-      email =  Notify.invite_message(@user, @from, @subject, @content)
+      email =  Notify.invite_message(@user, @from, @subject)
       email.deliver
       flash[:alert] = t("devise.invitations.new.email_sent")
     end
