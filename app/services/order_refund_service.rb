b7b8d2b722a33d9d3ff8387cd8e185c89caaf2cf
ryancheung <ryancheung.go@gmail.com>
2014-03-28 17:48:44 +0800

    Order of any state can do a reject_refund

--- app/services/order_refund_service.rb
+++ app/services/order_refund_service.rb
@@ -10,11 +10,12 @@ class OrderRefundService
   end
 
   def self.reject_refund(order, refund)
-    raise ArgumentError, "Invalid order state: #{order.state}" if order.state != 'wait_refund'
-
     ActiveRecord::Base.transaction do
       refund.reject
-      order.update_column(:state, order.has_shipped_shipment? ? 'wait_confirm' : 'wait_ship')
+
+      unless order.state == 'completed'
+        order.update_column(:state, order.has_shipped_shipment? ? 'wait_confirm' : 'wait_ship')
+      end
     end
   end
 end
