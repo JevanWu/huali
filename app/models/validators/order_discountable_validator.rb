a3e74655c3eeca378e9eaa94574b9ced660dbf0e
ryancheung <ryancheung.go@gmail.com>
2014-04-19 21:39:23 +0800

    Refined validator

--- app/models/validators/order_discountable_validator.rb
+++ app/models/validators/order_discountable_validator.rb
@@ -1,9 +1,9 @@
 class OrderDiscountableValidator < ActiveModel::Validator
   def validate(order)
-    if order.coupon_code 
-      order.line_items.each do |l|
-        if l.product.discountable == false
-          order.errors[:coupon_code] << "对不起! 您所选择的 '#{l.product.name_zh}' 是不能打折的!"
+    if order.coupon_code.present?
+      order.line_items.each do |item|
+        unless item.product.discountable
+          order.errors.add(:coupon_code, :product_not_discountable, product_name: item.product.name)
         end
       end
     end
