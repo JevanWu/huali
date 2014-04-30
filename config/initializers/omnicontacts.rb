00192ae66ea5ce5672e24eaeaf93d3136211cf09
ryancheung <ryancheung.go@gmail.com>
2014-04-30 16:22:12 +0800

    Use ENV store oauth secrets

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,5 +1,5 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "582486955899.apps.googleusercontent.com", "sC0kaKpw0AfWOq7lKZWgRj8P", { redirect_path: "/contact_callback" }
+  importer :gmail, ENV['GMAIL_CLIENT_ID'], ENV['GMAIL_CLIENT_SECRET'], { redirect_path: "/contact_callback" }
 end
