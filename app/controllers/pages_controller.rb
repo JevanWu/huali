93c1044e1887adc235f58d0b50a5c53ef3b543cf
jevanwu <jevanwu@gmail.com>
2014-05-16 16:21:44 +0800

    [chore] move the actions of the static pages to the bottom

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: :huali_point
 
+  def home
+    @menu_nav_type = 'home'
+    @slides = SlidePanel.visible
+  end
+
   def show
     @page = Page.find_by_permalink!(params[:id])
 
@@ -9,12 +14,45 @@ class PagesController < ApplicationController
     end
   end
 
-  def partner
+  def huali_point
+    @point_transactions = current_user.point_transactions.order(:created_at).page(params[:page]).per(3)
   end
 
-  def home
-    @menu_nav_type = 'home'
-    @slides = SlidePanel.visible
+  def refer_friend
+    @user = User.new
+  end
+
+  def email_signin
+    if params[:provider]
+      render "netease_signin"
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
+    @user = User.new
+  end
+
+  def contact_callback
+    retrieved_contacts = request.env['omnicontacts.contacts']
+    unless retrieved_contacts.nil?
+      @contacts = Array.new
+      retrieved_contacts.each do |c|
+        next if c[:email].nil?
+        contact = OpenStruct.new
+        name = c[:name].nil? ? c[:email] : c[:name]
+        contact.name = name
+        contact.email = c[:email]
+        @contacts << contact
+      end
+    end
+    @user = User.new
+    render :refer_friend
+    # @user = request.env['omnicontacts.user']
+  end
+  
+  #static pages
+  def partner
   end
 
   def brands
@@ -36,31 +74,5 @@ class PagesController < ApplicationController
   end
 
   def pick_up
-  end
-
-  def huali_point
-    @point_transactions = current_user.point_transactions.order(:created_at).page(params[:page]).per(3)
-  end
-
-  def refer_friend
-    @user = User.new
-  end
-
-  def contact_callback
-    retrieved_contacts = request.env['omnicontacts.contacts']
-    unless retrieved_contacts.nil?
-      @contacts = Array.new
-      retrieved_contacts.each do |c|
-        next if c[:email].nil?
-        contact = OpenStruct.new
-        name = c[:name].nil? ? c[:email] : c[:name]
-        contact.name = name
-        contact.email = c[:email]
-        @contacts << contact
-      end
-    end
-    @user = User.new
-    render :refer_friend
-    # @user = request.env['omnicontacts.user']
   end
 end
