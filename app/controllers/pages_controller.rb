5f8609c5fa0eac1f3791c208a2704ebf152adc59
jevanwu <jevanwu@gmail.com>
2014-05-19 18:37:29 +0800

    [feature] re-arrange the pages controller and users controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,4 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: :huali_point
-
-  def home
-    @menu_nav_type = 'home'
-    @slides = SlidePanel.visible
-  end
-
   def show
     @page = Page.find_by_permalink!(params[:id])
 
@@ -14,56 +7,12 @@ class PagesController < ApplicationController
     end
   end
 
-  def huali_point
-    @point_transactions = current_user.point_transactions.order(:created_at).page(params[:page]).per(3)
-  end
-
-  def refer_friend
-    @user = User.new
-  end
-
-  def email_signin
-    # if params[:provider]
-    render "netease_signin"
-    # else
-    #   raise ActionController::RoutingError.new('Not Found')
-    # end
-  end
-
-  def import_email_contacts
-    @username = params[:email]
-    @password = params[:passwd] 
-    importer = ContactsImporter::Netease.new(@username, @password)
-    if importer.success_login?
-      @contacts = importer.get_contacts
-    else
-      flash[:alert] = t("login_failed")
-      redirect_to email_signin_path and return
-    end
-    @user = User.new
-    render :refer_friend
-  end
-
-  def contact_callback
-    retrieved_contacts = request.env['omnicontacts.contacts']
-    unless retrieved_contacts.nil?
-      @contacts = Array.new
-      retrieved_contacts.each do |c|
-        next if c[:email].nil?
-        contact = OpenStruct.new
-        name = c[:name] || c[:email]
-        contact.name = name
-        contact.email = c[:email]
-        @contacts << contact
-      end
-    end
-    @user = User.new
-    render :refer_friend
-    # @user = request.env['omnicontacts.user']
-  end
-  
-  #static pages
   def partner
+  end
+
+  def home
+    @menu_nav_type = 'home'
+    @slides = SlidePanel.visible
   end
 
   def brands
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,69 @@
 class UsersController < ApplicationController
-  respond_to :json
+  before_action :authenticate_user!,
+    except: [:check_user_exist, :subscribe_email, :omnicontacts_callback, :omnicontacts_failure]
+  respond_to :json, :html
+
+  def edit_profile
+    @user = current_user
+  end
+
+  def update_profile
+    @user = User.find(current_user.id)
+
+    if @user.update_attributes(profile_params)
+      flash[:notice] = t("controllers.user.profile_updated")
+      redirect_to action: :edit_profile
+    else
+      render :edit_profile
+    end
+  end
+
+  def edit_account
+    @user = current_user
+  end
+
+  def update_password
+    @user = User.find(current_user.id)
+    if @user.update_with_password(password_params)
+      # Sign in the user by passing validation in case his password changed
+      sign_in @user, :bypass => true
+
+      flash[:notice] = t("controllers..user.password_updated")
+      redirect_to action: 'edit_account'
+    else
+      render "edit_account"
+    end
+  end
+
+  def huali_point
+    @point_transactions = current_user.point_transactions.order(:created_at).page(params[:page]).per(3)
+  end
+
+  def refer_friend
+    @user = User.new
+  end
+
+  def email_signin
+    # if params[:provider]
+    render "netease_signin"
+    # else
+    #   raise ActionController::RoutingError.new('Not Found')
+    # end
+  end
+
+  def import_email_contacts
+    @username = params[:email]
+    @password = params[:passwd] 
+    importer = ContactsImporter::Netease.new(@username, @password)
+    if importer.success_login?
+      @contacts = importer.get_contacts
+    else
+      flash[:alert] = t("login_failed")
+      redirect_to email_signin_path and return
+    end
+    @user = User.new
+    render :refer_friend
+  end
 
   def check_user_exist
     if u = User.find_by_email(check_user_params)
@@ -20,9 +84,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def omnicontacts_failure
+    @error_message = t("controllers.user.omnicontacts.#{params[:error_message]}")
+  end
+
   private
 
   def check_user_params
     params.require(:user_email)
+  end
+
+  def profile_params
+    params.required(:user).permit(:email, { phone: [] }, :name)
+  end
+
+  def password_params
+    params.required(:user).permit(:current_password, :password, :password_confirmation)
   end
 end
