a1f05aa8ed9416c90d029ee8e296daf9e83a4d29
ryancheung <ryancheung.go@gmail.com>
2014-06-19 03:02:22 +0800

    Use the correct branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'huali'
 set :repo_url, 'git@git.zenhacks.org:yangchenyun/huali.git'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, fetch(:stage) == :staging ? :staging : :master
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, "~/repositories/#{fetch(:application)}-#{fetch(:stage)}"
