4ef91ab2b3b4ddcd9adeb753e954645cfb11a622
ryancheung <ryancheung.go@gmail.com>
2014-06-24 18:12:32 +0800

    Update sitemap generater

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -32,13 +32,32 @@ SitemapGenerator::Sitemap.create do
   add '/brands', changefreq: 'monthly', priority: 0.33
   add '/celebrities', changefreq: 'monthly', priority: 0.33
   add '/medias', changefreq: 'monthly',  priority: 0.33
-  add '/blog', changefreq: 'daily', priority: 0.5 
+  add '/blog', changefreq: 'daily', priority: 0.5
   add '/muqinjie', changefreq: 'weekly', priority: 0.95
   add '/weibo_stories', changefreq: 'monthly', priority: 0.5
 
   I18n.locale = :'zh-CN'
 
+  promotion_collections = ['marc-jacobs', 'swarovski-elements'].map { |slug| Collection.friendly.find(slug) }
+  promotion_collections.each do |collection|
+    add collection_products_path(collection), lastmod: collection.updated_at, priority: 0.8
+  end
+
+  promotion_products = ['polka-dot--ab8e8fa1-0538-40a0-8955-8f9644ab4cf5',
+                        'the-girl-appreciation',
+                        'mother',
+                        'charming-dna',
+                        'the-key-of-the-forest'].map { |slug| Product.friendly.find(slug) }
+  promotion_products.each do |product|
+    product_images = product.assets.map do |asset|
+      { loc: asset.image.url(:medium), title: product.name }
+    end
+
+    add(product_path(product), lastmod: product.updated_at, images: product_images, priority: 0.65)
+  end
+
   Product.find_each do |product|
+    next if promotion_products.include?(product)
 
     product_images = product.assets.map do |asset|
       { loc: asset.image.url(:medium), title: product.name }
@@ -48,6 +67,8 @@ SitemapGenerator::Sitemap.create do
   end
 
   Collection.find_each do |collection|
+    next if promotion_collections.include?(collection)
+
     collection_images = []
     collection.products.each do |product|
       product.assets.map do |asset|
@@ -57,4 +78,5 @@ SitemapGenerator::Sitemap.create do
 
     add(collection_products_path(collection), lastmod: collection.updated_at, images: collection_images, priority: 0.5)
   end
+
 end
