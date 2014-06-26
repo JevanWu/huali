f0bd19940e451a7fa57adf0f218012189fbfa1c6
jevanwu <jevanwu@gmail.com>
2014-06-26 16:02:07 +0800

    change the js of nested form to coffeescript

--- app/helpers/postcards_helper.rb
+++ app/helpers/postcards_helper.rb
@@ -4,10 +4,10 @@ module PostcardsHelper
     field = f.simple_fields_for(association, new_object, child_index: "new_field") do |builder|
       render "asset_fields", f: builder
     end
-    link_to_function name, "add_fields(this, \"#{j(field)}\")"
+    link_to_function name, "Postcard.add_fields(this, \"#{j(field)}\")"
   end
 
   def link_to_remove_fields(name)
-    link_to_function name, "remove_fields(this)"
+    link_to_function name, "Postcard.remove_fields(this)"
   end
 end
