c3d49978f6a009c4df7979647dc2606bf045889b
ryancheung <ryancheung.go@gmail.com>
2014-06-27 11:00:22 +0800

    Update shipment info to ERP after shipment was saved

--- app/models/shipment_observer.rb
+++ app/models/shipment_observer.rb
@@ -2,4 +2,8 @@ class ShipmentObserver < ActiveRecord::Observer
   def after_ship(shipment, transition)
     Shipment.delay.kuaidi100_poll(shipment.id, $host || 'localhost') if shipment.is_express?
   end
+
+  def after_save(shipment)
+    ErpWorker::UpdateShipment.perform_async(shipment.id)
+  end
 end
