8776d5d7fb5577fbd123076090b244ac85f2df0b
ryancheung <ryancheung.go@gmail.com>
2014-07-04 11:59:27 +0800

    Export hightcharts as png format instead

--- lib/highcharts/converter.rb
+++ lib/highcharts/converter.rb
@@ -9,7 +9,7 @@ module Highcharts
     def self.run(chart_options)
       chart_options_file = Tempfile.new('chart_options')
 
-      image_filename = "#{SecureRandom.hex(32)}.svg"
+      image_filename = "#{SecureRandom.hex(32)}.png"
       sales_report_dir = File.join(::Rails.root, 'public', 'system', 'sales_report') and FileUtils.mkdir_p(sales_report_dir)
       outfile_path = File.join(sales_report_dir, image_filename) and FileUtils.touch(outfile_path)
       url_path = File.join('/', 'system', 'sales_report', image_filename)
@@ -17,7 +17,7 @@ module Highcharts
       begin
         chart_options_file.write(chart_options)
         chart_options_file.flush
-        system "#{phantomjs_path} #{File.expand_path('../highcharts-convert.js', __FILE__)} -infile #{chart_options_file.path} -outfile #{outfile_path} -type svg -constr Chart -width 1440"
+        system "#{phantomjs_path} #{File.expand_path('../highcharts-convert.js', __FILE__)} -infile #{chart_options_file.path} -outfile #{outfile_path} -type png -constr Chart -width 1440"
       ensure
         chart_options_file.close
         chart_options_file.unlink   # deletes the temp file
