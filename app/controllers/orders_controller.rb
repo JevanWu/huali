5d47a0392bf22fd08930a8a63072f3cc954b4139
jevanwu <jevanwu@gmail.com>
2014-07-11 14:23:16 +0800

    fix the error of orderscontroller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -103,11 +103,11 @@ class OrdersController < ApplicationController
         else
           flash[:notice] = t('controllers.order.order_success')
         end
-          if request.env["HTTP_USER_AGENT"].include? "MicroMessenger"
-            return and redirect_to checkout_order_path(@order_form.record, showwxpaytitle: 1)
-          else
-            return and redirect_to checkout_order_path(@order_form.record)
-          end
+        if request.env["HTTP_USER_AGENT"].include? "MicroMessenger"
+          redirect_to checkout_order_path(@order_form.record, showwxpaytitle: 1)
+        else
+          redirect_to checkout_order_path(@order_form.record)
+        end
       end
     else
       render 'new'
