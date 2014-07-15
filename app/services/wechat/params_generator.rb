b1111ade58fb711cf5f6cdb789800812285bab99
jevanwu <jevanwu@gmail.com>
2014-07-15 14:09:18 +0800

    service to generate params for wechat js payment

---
+++ app/services/wechat/params_generator.rb
@@ -0,0 +1,71 @@
+require 'uri'
+require 'digest'
+
+module Wechat
+  class ParamsGenerator
+    def self.get_appid
+      ENV["WECHAT_APPID"]
+    end
+
+    def self.get_package
+      banktype = "WX"
+      body = "fresh flower"
+      fee_type = "1"
+      input_charset = "GBK"
+      notify_url = "http://staging_wechat.zenhacks.org/wechats"
+      out_trade_no = "22399777777"
+      partner = ENV["WECHAT_PARTNERID"]
+      spbill_create_ip = "127.0.0.1"
+      total_fee = "1"
+      partnerKey = ENV["WECHAT_PARTNERKEY"] 
+    
+      signString = "bank_type="+banktype+"&body="+body+"&fee_type="+fee_type+"&input_charset="+input_charset+"&notify_url="+notify_url+"&out_trade_no="+out_trade_no+"&partner="+partner+"&spbill_create_ip="+spbill_create_ip+"&total_fee="+total_fee+"&key="+partnerKey
+    
+      md5SignValue = ("" + Digest::MD5.digest(signString)).upcase
+    
+      banktype = URI.escape(banktype)
+      body = URI.escape(body)
+      fee_type = URI.escape(fee_type)
+      input_charset = URI.escape(input_charset)
+      notify_url = URI.escape(notify_url)
+      out_trade_no = URI.escape(out_trade_no)
+      partner = URI.escape(partner)
+      spbill_create_ip = URI.escape(spbill_create_ip)
+      total_fee = URI.escape(total_fee)
+    
+      completeString = "bank_type="+banktype+"&body="+body+"&fee_type="+fee_type+"&input_charset="+input_charset+"&notify_url="+notify_url+"&out_trade_no="+out_trade_no+"&partner="+partner+"&spbill_create_ip="+spbill_create_ip+"&total_fee="+total_fee
+      completeString = completeString + "&sign=" + md5SignValue
+   
+      return completeString
+    end
+
+    def self.get_timestamp
+      timestampstring = Time.now.to_s
+    end
+
+    def self.get_nonce_str
+      chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+      maxPos = chars.length
+      noceStr = ""
+      for i in (0..32) 
+          noceStr += chars[Random.new.rand(0..maxPos)]
+      end
+      return noceStr
+    end
+
+    def self.get_signtype
+      return "SHA1"
+    end
+
+    def self.get_sign(nonce_str, package_str, timestamp)
+      app_id = ENV["WECHAT_APPID"]
+      app_key = ENV["WECHAT_APPKEY"] 
+      nonce_str = nonce_str 
+      package_string = package_str
+      time_stamp = timestamp
+      keyvaluestring = "appid="+app_id+"&appkey="+app_key+"&noncestr="+nonce_str+"&package="+package_string+"&timestamp="+time_stamp
+      sign = Digest::MD5.digest(keyvaluestring).to_s
+      return sign
+    end
+  end
+end
