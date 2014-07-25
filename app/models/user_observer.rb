7e3d3417c119ae01ec101fdc4dc5687d03f1a7b2
ryancheung <ryancheung.go@gmail.com>
2014-07-25 17:06:24 +0800

    Update tracking for user sign-up and sign-in

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,7 +1,10 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
     Utils.delay.subscribe_to_mailchimp(user.email, user.name)
-    AnalyticWorker.delay.identify_user(user.id)
-    GaTrackWorker.delay.user_sign_up_track(user.id)
+    Analytics.identify(user_id: user.id,
+                       traits: { name: user.name,
+                                 email: user.email,
+                                 phone: user.phone,
+                                 created_at: user.created_at })
   end
 end
