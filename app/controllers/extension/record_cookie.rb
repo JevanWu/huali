16373660137eb7eb83f1131871936468bd2a45ae
ryancheung <ryancheung.go@gmail.com>
2014-07-26 16:15:59 +0800

    Fix recording GA tracking client_id

--- app/controllers/extension/record_cookie.rb
+++ app/controllers/extension/record_cookie.rb
@@ -4,7 +4,7 @@ module Extension
 
     def touch_or_create_tracking_cookie(user)
       tracking_cookie = TrackingCookie.find_or_create_by(user_id: user.id)
-      tracking_cookie[:ga_client_id] = cookies['ga_client_id']
+      tracking_cookie[:ga_client_id] = cookies[:_ga].scan(/GA\d\.\d\.(.*)/)[0].first
       tracking_cookie.save
     end
   end
