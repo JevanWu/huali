7058274e9d17b9b9b9876918d81aa9bac922d5c3
ryancheung <ryancheung.go@gmail.com>
2014-07-26 15:39:48 +0800

    Tracking user sign-up event

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -6,5 +6,12 @@ class UserObserver < ActiveRecord::Observer
                                  email: user.email,
                                  phone: user.phone,
                                  created_at: user.created_at })
+
+    Analytics.track(user_id: user.id,
+                    event: 'Signed Up',
+                    properties: {
+                      label: user.email,
+                      category: 'User'
+                    })
   end
 end
