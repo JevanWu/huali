c37557b69048c52aab519a07c818c5527ac30af7
ryancheung <ryancheung.go@gmail.com>
2014-07-27 01:59:00 +0800

    Refactor RecordCookie extension

--- app/controllers/extension/record_cookie.rb
+++ app/controllers/extension/record_cookie.rb
@@ -4,8 +4,15 @@ module Extension
 
     def touch_or_create_tracking_cookie(user)
       tracking_cookie = TrackingCookie.find_or_create_by(user_id: user.id)
-      tracking_cookie[:ga_client_id] = cookies[:_ga].scan(/GA\d\.\d\.(.*)/)[0].first
+      tracking_cookie[:ga_client_id] = fetch_ga_client_id
       tracking_cookie.save
+    end
+
+    def fetch_ga_client_id
+      return if cookies[:_ga].nil?
+
+      cookies[:_ga] =~ /GA\d\.\d\.(.*)/
+      $1
     end
   end
 end
