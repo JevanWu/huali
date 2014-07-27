f07e20257b78e15089c59d08d4ed6b24405836c7
ryancheung <ryancheung.go@gmail.com>
2014-07-27 12:23:40 +0800

    Change session store from redis to cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Huali::Application.config.session_store :redis_store, key: '_huali_session'
+Huali::Application.config.session_store :cookie_store, key: '_huali_session'
