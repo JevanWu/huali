c132b03d53cff56f8886b923fb13d05da36dc7ce
ryancheung <ryancheung.go@gmail.com>
2014-07-27 10:47:12 +0800

    Fix not method for nil class error

--- app/models/order_observer.rb
+++ app/models/order_observer.rb
@@ -11,7 +11,7 @@ class OrderObserver < ActiveRecord::Observer
                       category: 'Order'
                     },
                     context: {
-                      'Google Analytics' => { clientId: order.user.tracking_cookie.ga_client_id }
+                      'Google Analytics' => { clientId: order.user.tracking_cookie.try(:ga_client_id) }
                     })
   end
 
@@ -38,7 +38,7 @@ class OrderObserver < ActiveRecord::Observer
                       city: order.city_name
                     },
                     context: {
-                      'Google Analytics' => { clientId: order.user.tracking_cookie.ga_client_id }
+                      'Google Analytics' => { clientId: order.user.tracking_cookie.try(:ga_client_id) }
                     })
   end
 
