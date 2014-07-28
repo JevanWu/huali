7474177a7bb0c4244d3e6cd4dc4f98d3fa9b92c8
ryancheung <ryancheung.go@gmail.com>
2014-07-28 12:11:17 +0800

    Refactor #touch_or_create_tracking_cookie

--- app/controllers/extension/record_cookie.rb
+++ app/controllers/extension/record_cookie.rb
@@ -3,9 +3,11 @@ module Extension
     extend ActiveSupport::Concern
 
     def touch_or_create_tracking_cookie(user)
-      tracking_cookie = TrackingCookie.find_or_create_by(user_id: user.id)
-      tracking_cookie[:ga_client_id] = fetch_ga_client_id
-      tracking_cookie.save
+      if user.tracking_cookie
+        user.tracking_cookie.update_column(:ga_client_id, fetch_ga_client_id)
+      else
+        user.create_tracking_cookie(ga_client_id: fetch_ga_client_id)
+      end
     end
 
     def fetch_ga_client_id
