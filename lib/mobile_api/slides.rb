831edea4b423dcd44f10e4bdb881bc990b3a4cfc
jevanwu <jevanwu@gmail.com>
2014-08-06 18:43:11 +0800

    deal with not found exception for slides

--- lib/mobile_api/slides.rb
+++ lib/mobile_api/slides.rb
@@ -5,6 +5,7 @@ module MobileAPI
      desc "Return all visible slides." 
      get do
        slides = SlidePanel.visible
+       error!('no visible slides exists for now!', 500) if slides.nil?
        res = Array.new
        slides.each do |slide|
          slide_info = { id: slide.id, name: slide.name, priority: slide.priority, href: slide.href, image: "#{slide.image ? slide.image.url : "/"}" }
@@ -19,6 +20,7 @@ module MobileAPI
      end
      get ":id" do
        slide = SlidePanel.find(params[:id])
+       error!('There is no such slide!', 400) if slide.nil?
        res = { id: slide.id, name: slide.name, priority: slide.priority, href: slide.href, image: "#{slide.image ? slide.image.url : "/"}" }
      end
     end
