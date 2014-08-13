c706b768fb157a315b82ae713d8d932f6e352688
jevanwu <jevanwu@gmail.com>
2014-08-13 17:58:56 +0800

    modify the code of collections api

--- lib/mobile_api/collections.rb
+++ lib/mobile_api/collections.rb
@@ -11,6 +11,21 @@ module MobileAPI
         end
         children_info
       end
+
+      def product_images(product)
+        res = []
+        product.assets.each do |asset|
+          asset_info = {
+            image_file_name: asset.image_file_name,
+            full: asset.image.url,
+            medium: asset.image.url(:medium),
+            small: asset.image.url(:small),
+            thumb: asset.image.url(:thumb)
+          }
+          res << asset_info
+        end
+        res
+      end
     end
 
     resource :collections do
@@ -18,13 +33,39 @@ module MobileAPI
       desc "Return all collections of products"
       get do
         top_collections = Collection.available.where(parent_id: nil)
-        error!('no available collection exists for now!', 500) if top_collections.nil?
+        error!('no available collection exists for now!', 404) if !top_collections.present?
         res = Array.new
         top_collections.each do |collection|
           collection_info = { id: collection.id, name_zh: collection.name_zh, name_en: collection.name_en, description: collection.description, priority: collection.priority, children_collections: children_collections(collection) } 
           res << collection_info
         end
         res
+      end
+
+      desc "Return all products of a collection"
+      get ":id/products" do
+        collection = Collection.find(params[:id])
+        error!('Cannot find the collection', 404) if !collection.present?
+        products = collection.products
+        error!('The collection has no products!', 404) if !products.present?
+        res = []
+        products.each do |product|
+          product_info  = { 
+            id: product.id, 
+            name_zh: product.name_zh, 
+            name_en: product.name_en, 
+            description: product.description, 
+            count_on_hand: product.count_on_hand, 
+            price: product.price, 
+            height: product.height, 
+            width: product.width, 
+            depth: product.depth, 
+            priority: product.priority, 
+            product_type: product.product_type, 
+            images: product_images(product) 
+          }
+          res << product_info
+        end
       end
     end
   end
