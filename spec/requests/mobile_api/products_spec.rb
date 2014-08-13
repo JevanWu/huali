fcdc145d78767829f453927f49d2588fee7a2a7a
jevanwu <jevanwu@gmail.com>
2014-08-13 18:10:48 +0800

    improve the test of products api

--- spec/requests/mobile_api/products_spec.rb
+++ spec/requests/mobile_api/products_spec.rb
@@ -2,26 +2,33 @@ require 'spec_helper'
 
 describe MobileAPI::API do
 
-  before do
-    FactoryGirl.create_list(:product, 3)
-  end
-
-  let(:required_product){ create(:product, name_en:"required product") }
+  let(:spring){ create(:product, name_en:"spring") }
 
   describe "GET /mobile_api/v1/products" do
-    it "return all published products" do
+    it "returns all published products" do
+      FactoryGirl.create_list(:product, 3)
       get "/mobile_api/v1/products"
       response.status.should == 200
       res_arr = JSON.parse response.body
       res_arr.count.should == 3
     end
+
+    it "returns 404 status if there is no products" do
+      get "/mobile_api/v1/products"
+      response.status.should == 404
+    end
   end
 
   describe "GET /mobile_api/products/:id" do
     it "return a products depends on the params[:id]" do
-      get "/mobile_api/v1/products/#{required_product.id}"
+      get "/mobile_api/v1/products/#{spring.id}"
       response.status.should == 200
-      response.body.should match("required product")
+      response.body.should match("spring")
+    end
+
+    it "return 404 status if the product cannot be found" do
+      get "/mobile_api/v1/products/1"
+      response.status.should == 404
     end
   end
 end
