70298ab2aed67c8f8f1721683e12ac956e33c4fa
jevanwu <jevanwu@gmail.com>
2014-08-14 18:16:02 +0800

    supplement the test of users api

--- spec/requests/mobile_api/users_spec.rb
+++ spec/requests/mobile_api/users_spec.rb
@@ -2,21 +2,57 @@ require 'spec_helper'
 
 describe MobileAPI::API do
 
-  let(:user){ create(:user, email: "jevan@hua.li", password: "12345678") }
+  let(:user){ create(:user, huali_point: 300.0) }
 
-  describe "GET /mobile_api/v1/users/sign_in" do
-    it "return user authentication token" do
+  describe "POST /mobile_api/v1/users/sign_in" do
+    it "returns user authentication token" do
       post "/mobile_api/v1/users/sign_in", email: user.email, password: user.password
       response.status.should == 200
       response.body.should match(user.authentication_token)
     end
+
+    it "returns 404 status if the user does not exit" do
+      post "/mobile_api/v1/users/sign_in", email: "jevan@hua.li", password: "12345678"
+      response.status.should == 404 
+    end
+
+    it "returns 500 status if the passowrd is wrong" do
+      post "/mobile_api/v1/users/sign_in", email: user.email, password: "12345678"
+      response.status.should == 500 
+    end
   end
 
-  # describe "GET /mobile_api/slides/:id" do
-  #   it "return a slide depends on the :id" do
-  #     get "/mobile_api/v1/slides/#{required_slide.id}"
-  #     response.status.should == 200
-  #     response.body.should match("required slide")
-  #   end
-  # end
+  describe "POST /users" do
+    it "registers a user" do
+      post "/mobile_api/v1/users", email: "jevan@hua.li", password: "12345678", name: "Jevan", phone: "18758161801" 
+      response.status.should == 201
+    end
+
+    it "returns 400 status if there is no parameter 'password'" do
+      post "/mobile_api/v1/users", email: "jevan@hua.li", password: "12345678", name: "Jevan", phone: "18758161801" 
+      response.status.should == 400 
+    end
+  end
+
+  describe "PUT /users/change_password" do
+    it "changes the password of user" do
+      put "/users/change_password", password: user.password, new_password: "12345678"
+      response.status.should == 200
+    end
+  end
+
+  describe "PUT /users" do
+    it "changes user information" do
+      put "/users", name: user.name, phone: "18758161801"
+      response.status.should == 200
+    end
+  end
+
+  describe "GET /users/huali_points" do
+    it "returns huali points of the user" do
+      get "/users/huali_points", id: user.id
+      response.status.should == 200
+      response.body.should match(user.huali_point)
+    end
+  end
 end
