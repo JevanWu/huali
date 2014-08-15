a177daa5d9749dca4f4d244c0f61bd6407d4f106
jevanwu <jevanwu@gmail.com>
2014-08-15 18:26:30 +0800

    improve the code of users api

--- lib/mobile_api/users.rb
+++ lib/mobile_api/users.rb
@@ -1,9 +1,6 @@
 module MobileAPI
   class Users < Grape::API
 
-    helpers do
-    end
-
     resource :users do
 
       desc "Return authentication token for identifying current user." 
@@ -13,8 +10,13 @@ module MobileAPI
       end
       post :sign_in do
         user = User.find_by(email: params[:email])
-        token = user.authentication_token if user.valid_password?(params[:password])
-        status 200 and return { authentication_token: token }
+        error!('The user does not exist!', 404) if !user.present?
+        if user.valid_password?(params[:password])
+          token = user.authentication_token
+          status 200 and return { authentication_token: token }
+        else
+          error!("The account and password don't match!", 500)
+        end
       end
 
       desc "User sign up and return authentication token for identifying current user." 
@@ -24,13 +26,55 @@ module MobileAPI
         requires :name, type: String, desc: "User's name."
         requires :phone, type: String, desc: "User's phone number."
       end
-      post :sign_up do
-        if params[:user_password] == params[:user_password_confirmation]
-          user = User.create(email: params[:user_email], password: params[:user_password], name: params[:user_name],
-                             phone: params[:phone]) 
-          token = user.authentication_token
-        end
-        { authentication_token: token }
+      post do
+        user = User.create(email: params[:email], password: params[:password], name: params[:name], phone: params[:phone], bypass_humanizer: true) 
+        error!(user.errors.messages, 500) if user.errors.messages.present?
+        { authentication_token: user.authentication_token }
+      end
+
+      desc "Change password for current user." 
+      params do
+        requires :password, type: String, desc: "Current password."
+        requires :new_password, type: String, desc: "New password"
+        requires :email, type: String, desc: "email of the user"
+        requires :token, type: String, desc: "Authentication token of the user"
+      end
+      put :change_password do
+        binding.pry
+        authenticate_user!
+        error!("Invalid password", 400) if current_user.valid_password?(params[:password])
+        current_user.password = params[:new_password]
+        current_user.save
+        error!(current_user.errors.messages, 500) if current_user.errors.messages.present?
+        status 200
+      end
+
+      desc "Edit user information." 
+      params do
+        requires :name, type: String, desc: "name of the user"
+        requires :phone, type: String, desc: "phone of the user"
+        requires :email, type: String, desc: "email of the user"
+        requires :token, type: String, desc: "Authentication token of the user"
+      end
+      put do
+        authenticate_user!
+        error!("No user signed in", 500) unless current_user
+        current_user.name = params[:name]
+        current_user.phone = params[:phone]
+        current_user.save
+        error!(current_user.errors.messages, 500) if current_user.errors.messages.present?
+        status 200
+      end
+
+      desc "Query the amount of huali point" 
+      params do
+        requires :email, type: String, desc: "email of the user"
+        requires :token, type: String, desc: "Authentication token of the user"
+      end
+      get :huali_points do
+        authenticate_user!
+        error!("No user signed in", 500) unless current_user
+        { huali_points: current_user.huali_point }
       end
     end
   end
