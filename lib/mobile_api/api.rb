b44934703f662c8d0eb87a72526dca1efc237eba
jevanwu <jevanwu@gmail.com>
2014-08-15 18:27:37 +0800

    modify the authenticate_user! helper method

--- lib/mobile_api/api.rb
+++ lib/mobile_api/api.rb
@@ -6,14 +6,17 @@ module MobileAPI
     format :json
 
     helpers do
+      attr_accessor :current_user
+
       def authenticate_user!
-        user_email = params[:user_email].presence
+        user_email = params[:email].presence
         user = user_email && User.find_by(email: user_email)
 
-        if user && Devise.secure_compare(user.authentication_token, params[:user_token])
-          sign_in user, store: false
+        if user && Devise.secure_compare(user.authentication_token, params[:token])
+          current_user = user
         end
       end
+
     end
 
     mount Users
