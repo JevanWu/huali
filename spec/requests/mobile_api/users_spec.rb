f3ff71b6f283064593ed5c8491d8cdc21744fde7
jevanwu <jevanwu@gmail.com>
2014-08-15 18:25:12 +0800

    modify the code of test of users api

--- spec/requests/mobile_api/users_spec.rb
+++ spec/requests/mobile_api/users_spec.rb
@@ -22,35 +22,35 @@ describe MobileAPI::API do
     end
   end
 
-  describe "POST /users" do
+  describe "POST /mobile_api/v1/users" do
     it "registers a user" do
       post "/mobile_api/v1/users", email: "jevan@hua.li", password: "12345678", name: "Jevan", phone: "18758161801" 
       response.status.should == 201
     end
 
-    it "returns 400 status if there is no parameter 'password'" do
-      post "/mobile_api/v1/users", email: "jevan@hua.li", password: "12345678", name: "Jevan", phone: "18758161801" 
-      response.status.should == 400 
+    it "returns 500 status if signing up failed" do
+      post "/mobile_api/v1/users", email: "jevan@hua.li", password: "12345678", name: "Jevan", phone: "1875" 
+      response.status.should == 500 
     end
   end
 
-  describe "PUT /users/change_password" do
+  describe "PUT /mobile_api/v1/users/change_password" do
     it "changes the password of user" do
-      put "/users/change_password", password: user.password, new_password: "12345678"
+      put "/mobile_api/v1/users/change_password", password: user.password, new_password: "12345678", email: user.email, token: user.authentication_token
       response.status.should == 200
     end
   end
 
-  describe "PUT /users" do
+  describe "PUT /mobile_api/v1/users" do
     it "changes user information" do
-      put "/users", name: user.name, phone: "18758161801"
+      put "/mobile_api/v1/users", name: user.name, phone: "18758161801", email: user.email, token: user.authentication_token
       response.status.should == 200
     end
   end
 
-  describe "GET /users/huali_points" do
+  describe "GET /mobile_api/v1/users/huali_points" do
     it "returns huali points of the user" do
-      get "/users/huali_points", id: user.id
+      get "/mobile_api/v1/users/huali_points", email: user.email, token: user.authentication_token
       response.status.should == 200
       response.body.should match(user.huali_point)
     end
