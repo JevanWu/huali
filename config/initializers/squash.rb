28285df7ee4a172a5eb94b28e7d991b8fda12878
ryancheung <ryancheung.go@gmail.com>
2014-09-09 18:24:25 +0800

    Update squash API Key

--- config/initializers/squash.rb
+++ config/initializers/squash.rb
@@ -1,2 +1,2 @@
 Squash::Ruby.configure api_host: 'http://squash.zenhacks.org',
-                         api_key: '397db31f-f6de-4162-acc9-b807ff219e25'
+                         api_key: '98b1897b-e4f7-4b6d-af7d-eb81d587e1df'
