5228d881f2fc02fb0a25ee5533711415f7abe31a
jevanwu <jevanwu@gmail.com>
2014-09-11 10:39:53 +0800

    add link to the last-child of breadcrumb

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -8,9 +8,11 @@ module BreadcrumbHelper
         link = link_to "#{col.name_zh}", collection_products_path(col)
         lies += content_tag(:li, link)
       end
-      lies += content_tag(:li, item.name_zh)
+      link = link_to item.name_zh, product_path(item)
+      lies += content_tag(:li, link)
     elsif item.is_a? Collection
-      lies += content_tag(:li, item.name_zh)
+      link = link_to item.name_zh, collection_products_path(item)
+      lies += content_tag(:li, link)
     end
 
     content_tag(:ol, lies, class: "breadcrumb")
