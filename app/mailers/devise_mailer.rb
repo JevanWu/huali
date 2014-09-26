0dd6aa9f1bfbbb6947f8430aa95b2aa6377b8582
jevanwu <jevanwu@gmail.com>
2014-09-26 14:24:52 +0800

    add emailcar header

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,3 +1,11 @@
 class DeviseMailer < Devise::Mailer
   default from: '"花里花店" <support@postmaster.hua.li>', content_type: 'text/html', css: 'email'
+  after_action :set_emailcar_header
+
+  private
+
+    def set_emailcar_header
+      headers["X-Scedm-Tid"] = "#{ENV["EMAILCAR_SMTP_USERNAME"]}.#{Time.current.to_i}"
+      headers["Reply-To"] = "support@hua.li"
+    end
 end
