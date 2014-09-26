7449842f253a84070d186895edd97fe63219b1a7
jevanwu <jevanwu@gmail.com>
2014-09-26 11:07:25 +0800

    make devise mailer async

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
+  devise :invitable, :async, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable,
          :omniauthable, :omniauth_providers => [:douban, :weibo, :qq_connect]
 
