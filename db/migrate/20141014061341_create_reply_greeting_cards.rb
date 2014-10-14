6dea19153d9508194ad5c1a746f5ad9d9c630523
Yanying <wangyanying1@gmail.com>
2014-10-14 16:10:34 +0800

    add fields to reply_greeting_card

--- db/migrate/20141014061341_create_reply_greeting_cards.rb
+++ db/migrate/20141014061341_create_reply_greeting_cards.rb
@@ -1,7 +1,8 @@
 class CreateReplyGreetingCards < ActiveRecord::Migration
   def change
     create_table :reply_greeting_cards do |t|
-
+      t.references :greeting_card, index: true
+      t.text :content
       t.timestamps
     end
   end
