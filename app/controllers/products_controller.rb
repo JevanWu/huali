fdc141d0e4d64ea254d7d9f11867908dea88adfd
Yanying <wangyanying1@gmail.com>
2014-10-21 20:14:07 +0800

    Notify.delay

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -116,7 +116,6 @@ class ProductsController < ApplicationController
       end
     end
     if @greeting_card.save and Notify.delay.product_greeting_card_email(@greeting_card)
-    #if @greeting_card.save and Notify.product_greeting_card_email(@greeting_card).deliver
       redirect_to product_path(@greeting_card.product), flash: { success: t("views.greeting_card.succeeded") }
     else
       redirect_to product_path(@greeting_card.product), flash: { fail: t("views.greeting_card.failed") }
@@ -126,7 +125,6 @@ class ProductsController < ApplicationController
   def reply_greeting_card
     @reply_greeting_card = ReplyGreetingCard.new(reply_greeting_card_params)
     if @reply_greeting_card.save and Notify.delay.product_reply_greeting_card_email(@reply_greeting_card)
-    #if @reply_greeting_card.save and Notify.product_reply_greeting_card_email(@reply_greeting_card).deliver
       redirect_to product_path(@reply_greeting_card.greeting_card.product), flash: { success: t("views.reply_greeting_card.succeeded") }
     else
       redirect_to product_path(@reply_greeting_card.greeting_card.product), flash: { fail: t("views.reply_greeting_card.failed") }
