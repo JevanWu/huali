4fcf116ee29952821e5c475c6bc6052c4c19bae9
Yanying <yanying.me@gmail.com>
2014-10-23 18:35:09 +0800

    refresh paperclip missing styles on production deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -81,7 +81,17 @@ namespace :deploy do
     end
   end
 
+  #refresh paperclip missing_styles
+  desc "build missing paperclip styles"
+  task :build_missing_paperclip_styles do
+    on roles(:app) do
+      execute "cd #{current_path}; RAILS_ENV=production bundle exec rake paperclip:refresh:missing_styles"
+    end
+  end
+
 end
+
+after("deploy:compile_assets", "deploy:build_missing_paperclip_styles")
 
 # CKEditor
 desc 'Copy ckeditor nondigest assets'
