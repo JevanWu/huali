a5882b34c0f372d4125aaab963e789d4e80abe39
Yanying <yanying.me@gmail.com>
2014-10-24 15:29:28 +0800

    fix: cant find bundle cmd

--- config/deploy.rb
+++ config/deploy.rb
@@ -85,7 +85,7 @@ namespace :deploy do
   desc "build missing paperclip styles"
   task :build_missing_paperclip_styles do
     on roles(:app) do
-      execute "cd #{current_path}; RAILS_ENV=production bundle exec rake paperclip:refresh:missing_styles"
+       execute "cd #{current_path}; RAILS_ENV=production $HOME/.rbenv/shims/bundle exec rake paperclip:refresh:missing_styles"
     end
   end
 
