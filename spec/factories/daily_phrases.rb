fad3608c742588dd83473b3503625c00d9041dd3
jevanwu <jevanwu@gmail.com>
2014-10-24 10:52:51 +0800

    anotation

--- spec/factories/daily_phrases.rb
+++ spec/factories/daily_phrases.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: daily_phrases
+#
+#  created_at         :datetime
+#  id                 :integer          not null, primary key
+#  image_content_type :string(255)
+#  image_file_name    :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  phrase             :text
+#  title              :string(255)
+#  updated_at         :datetime
+#
+
 FactoryGirl.define do
   factory :daily_phrase do
     title { Forgery(:lorem_ipsum).words(2) }
