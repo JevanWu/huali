8fd76109727b8a88c0febb702ec8001294b1cd56
ryancheung <ryancheung.go@gmail.com>
2014-11-02 20:32:06 +0800

    Display instant delivery form using ajax

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
   before_action :justify_wechat_agent, only: [:index, :current, :checkout, :gateway, :new, :create]
   before_action :fetch_related_products, only: [:back_order_create, :channel_order_create, :current, :apply_coupon]
   before_action :signin_with_openid, only: [:new, :index]
-  before_action :authenticate_user!, only: [:new, :index, :show, :create, :checkout, :cancel, :edit_gift_card, :update_gift_card]
+  before_action :authenticate_user!, only: [:new, :index, :show, :create, :checkout, :cancel, :edit_gift_card, :update_gift_card, :instant_delivery_status]
   before_action :authenticate_administrator!, only: [:back_order_new, :back_order_create, :channel_order_new, :channel_order_create]
   #before_action :fetch_transaction, only: [:return, :notify]
   skip_before_action :verify_authenticity_token, only: [:notify]
@@ -280,7 +280,11 @@ class OrdersController < ApplicationController
   end
 
   def instant_delivery_status
-    render json: { instant_delivery_available: InstantDelivery.used_count_today < 24 }
+    if InstantDeliveryCheckService.new(params[:city_id], params[:address]).check
+      render json: { instant_delivery_available: true } and return
+    end
+
+    render json: { instant_delivery_available: false }
   end
 
   private
