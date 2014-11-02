0f82b8314a18a61dcc65accfa1a437fdf2d6762f
ryancheung <ryancheung.go@gmail.com>
2014-11-02 21:06:42 +0800

    Update instant delivery validator, fix view logic

--- app/models/validators/instant_delivery_validator.rb
+++ app/models/validators/instant_delivery_validator.rb
@@ -1,7 +1,7 @@
 class InstantDeliveryValidator < ActiveModel::Validator
   def validate(order)
     if order.instant_delivery
-      if InstantDelivery.used_count_today >= 24
+      if !InstantDeliveryCheckService.new(order.address.city_id, order.address.address).check || InstantDelivery.used_count_today >= 24
         order.errors.add(:instant_delivery, :instant_delivery_not_available)
       end
     end
