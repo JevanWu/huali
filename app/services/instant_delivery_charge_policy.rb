cf3610132ba15bcc8502a536f1bbf6230fab9f08
ryancheung <ryancheung.go@gmail.com>
2014-11-04 17:39:37 +0800

    Update instant delivery extra fee

--- app/services/instant_delivery_charge_policy.rb
+++ app/services/instant_delivery_charge_policy.rb
@@ -8,8 +8,17 @@ class InstantDeliveryChargePolicy
 
   def apply
     if instant_delivery
-      order.create_instant_delivery(delivered_in_minutes: 30, fee: 100) unless order.instant_delivery
+      order.create_instant_delivery(delivered_in_minutes: 30, fee: fee_by_address) unless order.instant_delivery
       order.update_column(:total, order.total + order.instant_delivery.fee)
+    end
+  end
+
+  def fee_by_address
+    case order.address.area_id
+    when 795 # 黄浦区
+      50
+    else
+      30
     end
   end
 end
