4ec2a793e18e77115c3485420cba3fa5dac81625
YanyingWang <yanyingwangwang@gmail.com>
2014-11-10 17:39:13 +0800

    rake db:migrate

--- app/models/sync_order.rb
+++ app/models/sync_order.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: sync_orders
+#
+#  administrator_id  :integer
+#  created_at        :datetime
+#  id                :integer          not null, primary key
+#  kind              :string(255)
+#  merchant_order_no :string(255)
+#  order_id          :integer
+#  updated_at        :datetime
+#
+# Indexes
+#
+#  index_sync_orders_on_administrator_id  (administrator_id)
+#  index_sync_orders_on_order_id          (order_id)
+#
+
 class SyncOrder < ActiveRecord::Base
 end
--- spec/models/sync_order_spec.rb
+++ spec/models/sync_order_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: sync_orders
+#
+#  administrator_id  :integer
+#  created_at        :datetime
+#  id                :integer          not null, primary key
+#  kind              :string(255)
+#  merchant_order_no :string(255)
+#  order_id          :integer
+#  updated_at        :datetime
+#
+# Indexes
+#
+#  index_sync_orders_on_administrator_id  (administrator_id)
+#  index_sync_orders_on_order_id          (order_id)
+#
+
 require 'spec_helper'
 
 describe SyncOrder do
