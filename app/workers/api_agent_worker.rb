ff0d782b694fda28f5f2dbee036c92c95279c1b9
YanyingWang <yanyingwangwang@gmail.com>
2014-11-10 14:09:52 +0800

    add sync_order methed to ApiAgentWorker class

--- app/workers/api_agent_worker.rb
+++ app/workers/api_agent_worker.rb
@@ -18,6 +18,7 @@ class ApiAgentWorker
     def check_order(kind, merchant_order_no)
       api_client.check_order(kind, merchant_order_no)
     end
+
     def sync_order(kind, merchant_order_no)
       api_client.sync_order(kind, merchant_order_no)
     end
