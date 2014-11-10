37a0d8a241bf5ea81d4e96edfa569c9712169255
YanyingWang <yanyingwangwang@gmail.com>
2014-11-10 17:34:48 +0800

    add fields(kind, merchant_order_no, administrator_id, order_id) to SyncOrder

--- db/migrate/20141110091601_create_sync_orders.rb
+++ db/migrate/20141110091601_create_sync_orders.rb
@@ -1,6 +1,10 @@
 class CreateSyncOrders < ActiveRecord::Migration
   def change
     create_table :sync_orders do |t|
+      t.references :administrator, index: true
+      t.references :order, index: true
+      t.string :kind
+      t.string :merchant_order_no
       t.timestamps
     end
   end
