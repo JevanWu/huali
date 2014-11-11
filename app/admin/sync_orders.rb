4f596e98836b5ab4d666d3502db0c03b63c43106
YanyingWang <yanyingwangwang@gmail.com>
2014-11-11 14:44:33 +0800

    improve active_admin:sync_order:column:订单编号

--- app/admin/sync_orders.rb
+++ app/admin/sync_orders.rb
@@ -15,7 +15,9 @@ ActiveAdmin.register SyncOrder do
     column :administrator
     column :kind
     column :merchant_order_no
-    column "同步状态"
+    column "订单编号" do |sync_order|
+      sync_order.order ? link_to(sync_order.order.identifier, admin_order_path(sync_order.order)) : status_tag('正在同步')
+    end
     column :created_at
     column :updated_at
     actions
