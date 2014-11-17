089e7d15927899685ca5d8f25f5ff62017844af3
jevanwu <jevanwu@gmail.com>
2014-11-17 15:39:44 +0800

    add links for story api

--- lib/mobile_api/stories.rb
+++ lib/mobile_api/stories.rb
@@ -15,7 +15,7 @@ module MobileAPI
        error!('no weibo story exists for now!', 404) if !stories.present?
        res = Array.new
        stories.each do |story|
-         story_info = { id: story.id, name: story.name, description: story.description, picture: "#{story.picture ? story.picture.url : "/"}", author_avatar: "#{story.author_avatar ? story.author_avatar.url : "/"}" }
+         story_info = { id: story.id, name: story.name, description: story.description, picture: "#{story.picture ? story.picture.url : "/"}", author_avatar: "#{story.author_avatar ? story.author_avatar.url : "/"}", origin_link: story.origin_link, product_link: story.product_link }
          res << story_info
        end
        res
