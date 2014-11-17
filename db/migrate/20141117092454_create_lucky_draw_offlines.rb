8e3bfc2088d09cb352004e741a9988853027ed2f
YanyingWang <yanyingwangwang@gmail.com>
2014-11-17 17:43:34 +0800

    add fields to LyckyDrawOffline

--- db/migrate/20141117092454_create_lucky_draw_offlines.rb
+++ db/migrate/20141117092454_create_lucky_draw_offlines.rb
@@ -1,7 +1,9 @@
 class CreateLuckyDrawOfflines < ActiveRecord::Migration
   def change
     create_table :lucky_draw_offlines do |t|
-
+      t.string :gender
+      t.string :name
+      t.string :mobile
       t.timestamps
     end
   end
