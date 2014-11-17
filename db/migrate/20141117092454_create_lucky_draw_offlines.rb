d7b18dbba941d4537bc8cf19a5ad43f06c40db52
YanyingWang <yanyingwangwang@gmail.com>
2014-11-17 18:23:11 +0800

    add fields :prize and modify LuckyDrawOffline fields: :mobile to be integer

--- db/migrate/20141117092454_create_lucky_draw_offlines.rb
+++ db/migrate/20141117092454_create_lucky_draw_offlines.rb
@@ -3,7 +3,8 @@ class CreateLuckyDrawOfflines < ActiveRecord::Migration
     create_table :lucky_draw_offlines do |t|
       t.string :gender
       t.string :name
-      t.string :mobile
+      t.integer :mobile
+      t.string :prize
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117092454) do
+ActiveRecord::Schema.define(version: 20141113084935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -326,14 +326,6 @@ ActiveRecord::Schema.define(version: 20141117092454) do
 
   add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
   add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
-
-  create_table "lucky_draw_offlines", force: true do |t|
-    t.string   "gender"
-    t.string   "name"
-    t.string   "mobile"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "monthly_solds", force: true do |t|
     t.integer  "sold_year"
