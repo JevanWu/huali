ea6d0fe1070d08a1dbdf4512f3ea3d4ea288b828
YanyingWang <yanyingwangwang@gmail.com>
2014-11-17 18:56:59 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Huali::Application.routes.draw do
     mount Sidekiq::Web, at: "/admin/sidekiq", as: :sidekiq
   end
 
+  get "lucky_draw_offline", to: 'lucky_draw_offlines#new'
+  post "lucky_draw_offline", to: 'lucky_draw_offlines#create'
+  get "lucky_draw_offline_result", to: 'lucky_draw_offlines#show'
+
   get "areas/:area_id", to: 'areas#show'
   get "cities/:city_id/areas", to: 'areas#index'
   get "cities/:city_id/areas/available_for_products", to: 'areas#available_for_products'
