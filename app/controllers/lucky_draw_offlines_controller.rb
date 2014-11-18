59746d8626dca81981603e6c8347ffdd557d6f87
YanyingWang <yanyingwangwang@gmail.com>
2014-11-18 13:56:28 +0800

    add create to controller

--- app/controllers/lucky_draw_offlines_controller.rb
+++ app/controllers/lucky_draw_offlines_controller.rb
@@ -3,9 +3,17 @@ class LuckyDrawOfflinesController < ApplicationController
     @lucky_draw_offline = LuckyDrawOffline.new
   end
   def create
-
+    binding.pry and return
+    @lucky_draw_offline = LuckyDrawOffline.new(lucky_draw_offline_params)
+    LuckyDrawOfflinePrizeGenerator.init
+    @lucky_draw_offline.prize = LuckyDrawOfflinePrizeGenerator.generate
   end
   def show
 
+  end
+
+  private
+  def lucky_draw_offline_params
+    params.require(:lucky_draw_offline).permit(:gender, :name, :mobile)
   end
 end
