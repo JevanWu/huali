85fbc8818221cae916eaf06211c60eb5e9ef4185
YanyingWang <yanyingwangwang@gmail.com>
2014-11-18 10:56:39 +0800

    add fields to *PrizeGenerator

--- db/migrate/20141117110614_create_lucky_draw_offline_prize_generators.rb
+++ db/migrate/20141117110614_create_lucky_draw_offline_prize_generators.rb
@@ -1,7 +1,7 @@
 class CreateLuckyDrawOfflinePrizeGenerators < ActiveRecord::Migration
   def change
     create_table :lucky_draw_offline_prize_generators do |t|
-
+      t.text :collection
       t.timestamps
     end
   end
