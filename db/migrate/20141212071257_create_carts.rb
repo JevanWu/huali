5f518c71e6cfbf9deade00c63c8be828363de072
YanyingWang <yanyingwangwang@gmail.com>
2014-12-15 18:07:25 +0800

    remove field: product_id

--- db/migrate/20141212071257_create_carts.rb
+++ db/migrate/20141212071257_create_carts.rb
@@ -1,11 +1,8 @@
 class CreateCarts < ActiveRecord::Migration
   def change
     create_table :carts do |t|
-
       t.references :user, index: true
-      t.references :product, index: true
-      t.decimal :price, precision: 8, scale: 2, null: false
-      t.integer :quantity, null: false
+      t.decimal :total_price, precision: 8, scale: 2, null: false
       t.string :coupon_code
       t.timestamps
     end
