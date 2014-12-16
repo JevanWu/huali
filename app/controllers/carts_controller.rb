9c1861f9a22e7fa865e0a2363040d3d4da0b306f
YanyingWang <yanyingwangwang@gmail.com>
2014-12-16 15:32:11 +0800

    modify controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,17 +1,20 @@
 class CartsController < ApplicationController
   def create
-    @cart = Cart.new(cart_params)
-    redirect_to 'show' if @cart.save
+    @cart = Cart.where(user_id: cart_params[:user_id]).first
+    @cart = Cart.new(user_id: cart_params[:user_id]) if @cart.nil?
+    @cart.cart_line_items.new(product_id: cart_params[:product_id],
+                         quantity: cart_params[:quantity],
+                         price: cart_params[:price])
+    @cart.total_price = @cart.cart_line_items.map(&:price).inject(:+)
+    redirect_to carts_show_path if @cart.save
   end
   def show
-    @carts = Cart.where(user_id: current_user.id)
-    if @carts.present?
-      @product = Product.published.find(@carts.first.product_id)
+    @cart = Cart.where(user_id: current_user.id).first
+    if @cart.present?
+      @product = Product.published.find(@cart.cart_line_items.first.product_id)
       @related_products = @product.related_products
     end
   end
-
-
 
 
   def cart_params
