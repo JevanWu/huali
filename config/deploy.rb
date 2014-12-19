ae97efabae8affe4063a018b988fc4f0e010f596
jevanwu <jevanwu@gmail.com>
2014-12-19 11:48:46 +0800

    change to jungle restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,9 +59,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :service, "unicorn_huali upgrade"
-      invoke "puma:restart"
+      invoke "puma:jungle:restart"
     end
   end
 
