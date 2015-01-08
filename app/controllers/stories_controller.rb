c43d16d402cbc0f89650163aca63604c6628c682
ryancheung <ryancheung.go@gmail.com>
2015-01-08 12:39:31 +0800

    Cache weibo stories page

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,17 +1,21 @@
 class StoriesController < ApplicationController
   def index
-    @stories = Story.available.order('priority').page(params[:page]).per(8)
-    @result = @stories.map do |story|
-      {
-        description: story.description,
-        picture_url: story.picture.url(:medium),
-        author_avatar_url: story.author_avatar.url(:small),
-        width: 200,
-        height: 260,
-        origin_link: story.origin_link
-      }
+    cache_key = "stories/all-#{Story.available.count}-#{Story.available.maximum(:updated_at).try(:utc).try(:to_s, :number)}/page/#{params[:page]}"
+    raise ArgumentError, "Wrong page number" if params[:page].to_i * 8 > 800 # Only display 100 pages of stories
+    stories_json = Rails.cache.fetch(cache_key) do
+      @stories = Story.available.order('priority').page(params[:page]).per(8)
+      @result = @stories.map do |story|
+        {
+          description: story.description,
+          picture_url: story.picture.url(:medium),
+          author_avatar_url: story.author_avatar.url(:small),
+          width: 200,
+          height: 260,
+          origin_link: story.origin_link
+        }
+      end.to_json
     end
 
-    render :json => @result.to_json, :callback => params[:callback]
+    render :json => stories_json, :callback => params[:callback]
   end
 end
