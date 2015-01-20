3f1aa5e20320cd9ebb8387d9f165b5f2871e68cf
YanyingWang <yanyingwangwang@gmail.com>
2015-01-20 18:19:52 +0800

    create_order  of quick purchase

--- app/controllers/quick_purchases_controller.rb
+++ app/controllers/quick_purchases_controller.rb
@@ -5,11 +5,12 @@ class QuickPurchasesController < ApplicationController
     @quick_purchase_form.sender = SenderInfo.new
     @quick_purchase_form.address = ReceiverInfo.new
   end
+
   def create_address
     @quick_purchase_form = QuickPurchaseForm.new(params[:quick_purchase_form])
     if @quick_purchase_form.sender.valid? and @quick_purchase_form.address.valid?
       session[:quick_purchase_form] = @quick_purchase_form
-      redirect_to root_path, notice: "地址已更新"
+      redirect_to products_quick_purchase_path, notice: "地址已添加"
     else
       redirect_to new_address_quick_purchase_path
     end
@@ -17,30 +18,47 @@ class QuickPurchasesController < ApplicationController
     #@quick_purchase_form.kind = :quick_purchase
   end
 
-  def new_products
-    # validate address
-    if session[:quick_purchase_form].nil? and session[:quick_purchase_form] && session[:quick_purchase_form].empty?
-      redirect_to new_address_quick_purchase_path, notice: "请填写地址" 
+  def products
+    # validate quick_qurchase_form
+    unless session[:quick_purchase_form]
+      redirect_to new_address_quick_purchase_path, notice: "请先填写地址" 
       return 
     end
 
-    @quick_purchase_form = session[:quick_purchase_form]
+    @cart_cookies = cookies[:cart] ? JSON.parse(cookies[:cart]) : nil
 
+    expected_date = session[:quick_purchase_form].expected_date.to_s
+    area_id = session[:quick_purchase_form].address.area_id.to_s
+    product_ids_regin = ( DefaultRegionRule.get_product_ids_by(area_id) | LocalRegionRule.get_product_ids_by(area_id) )
+    product_ids_date = ( DefaultDateRule.get_product_ids_by(expected_date) | LocalDateRule.get_product_ids_by(expected_date) )
+    product_ids = product_ids_regin & product_ids_date
+
+    @products = Product.published.where("id IN (?) and count_on_hand > 0", product_ids).page(params[:page]).per(4).order_by_priority
+  end
+
+  def create_order
+    quick_purchase_form = session[:quick_purchase_form]
+    quick_purchase_form.user = current_or_guest_user
+
+    cart_cookies = JSON.parse(cookies[:cart]) 
+    cart_cookies.each do |product_id, quantity|
+      quick_purchase_form.add_line_item(product_id, quantity)
+    end
+
+    if quick_purchase_form.save
+      empty_quick_purchase
+      empty_cart
+      redirect_to checkout_order_path(quick_purchase_form.record)  
+    end
   end
 
   private
-  def get_quick_purchase_session
-    @quick_purchase_form = if session[:quick_purchase_form] and !session[:quick_purchase_form].empty?
-                               JSON.parse(cookies[:quick_purchase])
-                             else
-                               {}
-                             end
+  def empty_quick_purchase
+    session.delete(:quick_purchase_form)
   end
-  def save_quick_purchase_cookies
-    cookies[:quick_purchase] = JSON.generate(@quick_purchase_cookies) if @quick_purchase_cookies
-  end
-  def empty_quick_purchase_cookies
-    cookies[:quick_purchase] = {}
+  def empty_cart
+    cookies.delete :cart
+    cookies.delete :coupon_code
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -71,8 +71,8 @@ Huali::Application.routes.draw do
   get 'quick_purchases/address/new', to: 'quick_purchases#new_address', as: :new_address_quick_purchase
   post 'quick_purchases/address/create', to: 'quick_purchases#create_address', as: :create_address_quick_purchase
 
-  get 'quick_purchases/products/new', to: 'quick_purchases#new_products', as: :new_products_quick_purchase
-  get 'quick_purchases/products/create', to: 'quick_purchases#save_products', as: :create_products_quick_purchase
+  get 'quick_purchases/products', to: 'quick_purchases#products', as: :products_quick_purchase
+  post 'quick_purchases/order/create', to: 'quick_purchases#create_order', as: :create_order_quick_purchase
 
   # back order urls
   get 'orders/backorder', to: 'orders#back_order_new', as: :new_back_order
