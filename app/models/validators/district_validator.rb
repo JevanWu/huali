79cc802e6dbf43240e4c888c64aaae93e08b91d1
jevanwu <jevanwu@gmail.com>
2015-01-22 17:52:56 +0800

    add district validation error to base errors

--- app/models/validators/district_validator.rb
+++ app/models/validators/district_validator.rb
@@ -1,7 +1,7 @@
 class DistrictValidator < ActiveModel::Validator
   def validate(order)
-      order.errors.add(:address, :district_not_valid)
     if !DistrictCheckService.new(order.address.city_id, order.address.address).inside?($inner_zone)
+      order.errors.add(:base, :district_not_valid)
     end
   end
 end
