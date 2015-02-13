a0f69ee6550168a095cee1265e5282f70ed904f6
lifan <psylg910323@gmail.com>
2015-02-13 11:05:04 +0800

    fronted changes

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,30 +3,13 @@ $(document).ready(function(){
     if ($(".filter").css("display") == 'none')
     {
       $(".filter").slideDown(200);
-      
-      $(document).click(function(){ 
-        $(".filter").slideUp(200);
-      e.stopPropagation();
-      });
     }
     else
     {
       $(".filter").slideUp(200);
     }
   });
-  $(".btn-filter,.filter").on("click", function(e){
-    e.stopPropagation();
+  $(document).not(.filter).click(function(){
+      $(".filter").slideUp(200);
   });
-  $(".type-link,.color-link,.price-link").click(function(){
-  	 $(".product-type,.product-color,.product-price").hide(0);
-  }); 
-  $(".type-link").click(function(){
-  	 $(".product-type").show(0);
-  }); 
-  $(".color-link").click(function(){
-  	 $(".product-color").show(0);
-  }); 
-  $(".price-link").click(function(){
-  	 $(".product-price").show(0);
-  }); 
 });
