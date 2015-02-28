6eceedab3bba9cb0079d1d6ed799e8671eca84bf
lifan <psylg910323@gmail.com>
2015-02-28 16:57:05 +0800

    filter done.

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -2,6 +2,7 @@
 //= require 'jquery_ujs'
 //= require 'jquery.scrollTo'
 //= require 'jquery.SuperSlide.2.1.1'
+//= require 'jquery.cookie'
 //= require 'lrtk'
 //= require 'home_slide'
 //= require 'brand'
--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,13 +1,42 @@
 $(document).ready(function(){
+  var filtershow=$.cookie("filter");
+  if (filtershow == 0)
+  {
+    $(".filter").slideUp(0);
+  }
+  else
+  {
+    $(".filter").slideDown(0);
+  }
+
+
   $(".btn-filter").click(function(e){ 
     if ($(".filter").css("display") == 'none')
     {
       $(".filter").slideDown(200);
+      $.cookie("filter", "1"); 
+      $(document).click(function(){ 
+        $(".filter").slideUp(200);
+        e.stopPropagation();
+        $.cookie("filter", "0");
+      });
+
     }
     else
     {
       $(".filter").slideUp(200);
+      $.cookie("filter", "0"); 
     }
   });
-
+   $(".btn-filter,.filter").on("click", function(e){
+     e.stopPropagation();
+   });
+  $(document).click(function(){ 
+    $(".filter").slideUp(200);
+    e.stopPropagation();
+    $.cookie("filter", "0");
+  });
+  $(".btn-filter,.filter").on("click", function(e){
+     e.stopPropagation();
+   });
 });
