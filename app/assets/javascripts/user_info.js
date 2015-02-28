cd84fb1d7394a3787aa144e275681d65fc4b2caa
jevanwu <jevanwu@gmail.com>
2015-02-28 18:18:06 +0800

    fix error

--- app/assets/javascripts/user_info.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $(".tab-list li").removeClass(); 
-  var userpage=$.cookie("userpage");
-  $('#'+userpage).addClass("now")
-  $.cookie("userpage", "<%=@partial%>");
-});
