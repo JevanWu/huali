af83a6bcd5875a2d4d95a98e809a046a555934cb
jevanwu <jevanwu@gmail.com>
2015-03-02 15:12:07 +0800

    featured products functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,7 @@ class PagesController < ApplicationController
     @menu_nav_type = 'home'
     @slides = SlidePanel.visible
     @products = Product.published
+    @featured_products = FeaturedProduct.all
     if params[:coupon_code].present? && cookies[:coupon_code] != params[:coupon_code]
       coupon = CouponCode.find_by(code: params[:coupon_code])
       if coupon.usable?
