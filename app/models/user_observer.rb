04da7cc7916332b2299d20a26042d848e887b6f6
jevanwu <jevanwu@gmail.com>
2015-03-11 15:36:37 +0800

    comment out useless code

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,5 +1,5 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
-    Utils.delay.subscribe_to_mailchimp(user.email, user.name)
+    # Utils.delay.subscribe_to_mailchimp(user.email, user.name)
   end
 end
