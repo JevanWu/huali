189676e49f9c37164908b6e63bedf94cbdd6e2bc
jevanwu <jevanwu@gmail.com>
2015-03-26 11:42:05 +0800

    fix error

--- app/models/product_observer.rb
+++ app/models/product_observer.rb
@@ -4,9 +4,11 @@ class ProductObserver < ActiveRecord::Observer
   end
 
   def after_update(product)
-    changed = product.changed_attributes.except(:updated_at, :administrator_id, :tag_list, :trait_list, :color_list)
+    %w(updated_at, administrator_id, tag_list, trait_list, color_list).each do |attribute|
+      product.changed_attributes.delete(attribute)
+    end
     admin = product.administrator
-    changed.each do |attribute|
+    product.changed_attributes.each do |attribute|
       AdminOperation.create(administrator: admin, action: "modify", product: product, info: attribute[0], result: attribute[1])
     end
   end
