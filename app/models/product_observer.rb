4c5a6566379a87ae4ac4514895c1207836e7a084
jevanwu <jevanwu@gmail.com>
2015-03-26 11:24:22 +0800

    improve the code of monitoring admin operation

--- app/models/product_observer.rb
+++ app/models/product_observer.rb
@@ -4,10 +4,9 @@ class ProductObserver < ActiveRecord::Observer
   end
 
   def after_update(product)
-    product.changed_attributes.delete("updated_at")
-    product.changed_attributes.delete("administrator_id")
+    changed = product.changed_attributes.except(:updated_at, :administrator_id, :tag_list, :trait_list, :color_list)
     admin = product.administrator
-    product.changed_attributes.each do |attribute|
+    changed.each do |attribute|
       AdminOperation.create(administrator: admin, action: "modify", product: product, info: attribute[0], result: attribute[1])
     end
   end
